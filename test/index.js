5a9b3fe3b9189f2cad4842dac218d8ba53a7dd50
hveldstra <h@veldstra.org>
2015-07-10 15:28:16 +0100

    Disable incomplete tests

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-require('./test_worker_http');
+//require('./test_worker_http');
 require('./test_complete');
-require('./test_capture');
-require('./test_think');
+//require('./test_capture');
+//require('./test_think');
