078a28f1461d416a142d20210e573a0277308e23
hveldstra <h@veldstra.org>
2015-12-14 12:33:13 +0000

    Feature: "pause" and "arrivalCount" support in config.phases
    {"pause": 10} - pause for 10 seconds
    {"duration": 60, "arrivalCount": 50} - 50 arrivals in 60 seconds

---
+++ test/test_arrivals.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const test = require('tape');
+const runner = require('../lib/runner').runner;
+
+test('arrival phases', function(t) {
+  var script = require('./scripts/arrival_phases.json');
+
+  var ee = runner(script);
+
+  ee.on('phaseStarted', function(info) {
+    console.log('Starting phase: %j - %s', info, new Date());
+  });
+  ee.on('phaseCompleted', function() {
+    console.log('Phase completed - %s', new Date());
+  });
+
+  ee.on('done', function(stats) {
+    console.log('%j', stats);
+    t.end();
+  });
+  ee.run();
+});
