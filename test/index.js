af4c0e98f1e0761650afe8da6b90a6167dd976d1
hveldstra <h@veldstra.org>
2016-01-08 10:55:13 +0000

    Feature: allow multiple payload definitions
    script.config.payload can now be either an object (current behavior) or an array
    of objects with these attributes:
    
    - path - path to CSV file, relative to the test script
    - fields - CSV field names

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 require('./unit/index');
+require('./test_multiple_payloads');
+require('./test_complete');
 
 //require('./test_worker_http');
 //require('./test_environments.js');
-require('./test_complete');
 //require('./test_capture');
 //require('./test_arrivals');
 //require('./test_think');
---
+++ test/test_multiple_payloads.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const test = require('tape');
+const runner = require('../lib/runner').runner;
+const l = require('lodash');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parse');
+const async = require('async');
+
+test('multiple_payloads', function(t) {
+  const fn = './scripts/multiple_payloads.json';
+  let script = require(fn);
+
+  async.map(
+    script.config.payload,
+    function(item, callback) {
+      let payloadFile = path.resolve(
+        path.dirname(path.join(__dirname, fn)),
+        item.path);
+
+      let data = fs.readFileSync(payloadFile, 'utf-8');
+      csv(data, function(err, parsedData) {
+        item.data = parsedData;
+        return callback(err, item);
+      });
+    },
+    function(err, results) {
+      if (err) {
+        console.log(err);
+        t.fail(err);
+      }
+
+      let ee = runner(script, script.config.payload, {});
+
+      ee.on('phaseStarted', function(x) {
+        t.ok(x, 'phaseStarted event emitted');
+      });
+
+      ee.on('phaseCompleted', function(x) {
+        t.ok(x, 'phaseCompleted event emitted');
+      });
+
+      ee.on('stats', function(stats) {
+        t.ok(stats, 'intermediate stats event emitted');
+      });
+
+      ee.on('done', function(stats) {
+        let requests = stats.aggregate.requestsCompleted;
+        let scenarios = stats.aggregate.scenariosCompleted;
+        t.assert(stats.aggregate.codes[404] > 0, 'There are some 404s (URLs constructed from pets.csv)');
+        t.assert(stats.aggregate.codes[200] > 0, 'There are some 200s (URLs constructed from urls.csv)');
+        t.assert(stats.aggregate.codes[201] > 0, 'There are some 201s (POST with valid data from pets.csv)');
+        t.end();
+      });
+
+      ee.run();
+    });
+});
