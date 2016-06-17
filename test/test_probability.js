8bfc38f5c6490aa708a88aee31ff6e164503ef1f
hveldstra <h@veldstra.org>
2016-06-17 17:22:57 +0100

    HTTP engine: add probability option for requests

---
+++ test/test_probability.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const test = require('tape');
+const runner = require('../lib/runner').runner;
+const L = require('lodash');
+
+test('request probability', (t) => {
+  const script = require('./scripts/probability.json');
+
+  let ee = runner(script);
+  ee.on('done', (stats) => {
+    let requests = stats.aggregate.requestsCompleted;
+    t.assert(requests < 130,
+             'Should have completed ~10% = ~100 requests in total, actually completed ' + requests);
+    t.end();
+  });
+  ee.run();
+});
