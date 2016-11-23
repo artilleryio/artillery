4fa1220520d32c2a7e4fccd5b79776630b3a8db3
hveldstra <h@veldstra.org>
2016-11-23 20:59:43 +0000

    Allow user and pass for HTTP Basic Auth to be templated

--- test/index.js
+++ test/index.js
@@ -10,6 +10,7 @@ require('./test_probability');
 require('./test_if');
 require('./ws/test_options');
 require('./test_think');
+require('./test_basic_auth');
 
 //require('./test_worker_http');
 //require('./test_environments.js');
