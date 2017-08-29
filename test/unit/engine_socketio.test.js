bb7ed69c200e5b3153e79926e8b7cf5a3f8aec25
hveldstra <h@veldstra.org>
2017-08-29 14:16:05 +0100

    test(socketio): Add tests for Socket.io
    - Simple engine interface test
    - Test connecting and listening passively

--- test/unit/engine_socketio.test.js
+++ test/unit/engine_socketio.test.js
@@ -8,9 +8,11 @@ const EventEmitter = require('events');
 const test = require('tape');
 const SocketIoEngine = require('../../lib/engine_socketio');
 
+const createServer = require('../targets/simple_socketio');
+
 const script = {
   config: {
-    target: 'http://localhost:8888'
+    target: 'http://localhost:10333'
   },
   scenarios: [
     {
@@ -26,13 +28,52 @@ const script = {
     }
   ]
 };
+
+const scriptWithoutEmits = {
+  config: {
+    target: 'http://localhost:10334'
+  },
+  scenarios: [{
+    flow: [
+      { think: 1 }
+    ]
+  }]
+};
+
 test('SocketIo enginge interface', function(t) {
-  const engine = new SocketIoEngine(script);
-  const ee = new EventEmitter();
+  const target = createServer();
 
-  const runScenario = engine.createScenario(script.scenarios[0], ee);
+  target.listen(10333, function() {
+    const engine = new SocketIoEngine(script);
+    const ee = new EventEmitter();
 
-  t.assert(engine, 'Can init the engine');
-  t.assert(typeof runScenario === 'function', 'Can create a virtual user function');
-  t.end();
+    const runScenario = engine.createScenario(script.scenarios[0], ee);
+
+    t.assert(engine, 'Can init the engine');
+    t.assert(typeof runScenario === 'function', 'Can create a virtual user function');
+
+    target.close();
+    t.end();
+  });
+});
+
+test('Passive listening', function(t) {
+  const target = createServer();
+  target.listen(10334, function() {
+    const engine = new SocketIoEngine(scriptWithoutEmits);
+    const ee = new EventEmitter();
+
+    const runScenario = engine.createScenario(scriptWithoutEmits.scenarios[0], ee);
+    const initialContext = {
+      vars: {}
+    };
+
+    runScenario(initialContext, function userDone(err, finalContext) {
+      t.assert(!err, 'Scenario completed with no errors');
+      t.assert(finalContext.__receivedMessageCount === 1, 'Received one message upon connecting');
+
+      t.end();
+      target.close();
+    });
+  });
 });
