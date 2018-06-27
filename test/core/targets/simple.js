49cd33984af7b5aefa06388e3945e024cca466d7
hveldstra <h@veldstra.org>
2018-06-27 17:11:39 +0100

    test: Upgrade target for latest Hapi

--- test/core/targets/simple.js
+++ test/core/targets/simple.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var Hapi = require('hapi');
-var uuid = require('uuid');
-var Bcrypt = require('bcrypt');
-var Basic = require('hapi-auth-basic');
+const Hapi = require('hapi');
+const uuid = require('uuid');
+const Bcrypt = require('bcrypt');
+const Basic = require('hapi-auth-basic');
 
-var PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
-var REQUEST_COUNT = 0;
-var COOKIES = {};
+let REQUEST_COUNT = 0;
+let COOKIES = {};
 
-var server = new Hapi.Server({
-  load: {sampleInterval: 1000}
-});
+// const server = new Hapi.Server({
+//   load: { sampleInterval: 1000 }
+// });
 
-server.connection({
-  host: '0.0.0.0',
-  port: PORT
-});
+// server.connection({
+//   host: '0.0.0.0',
+//   port: PORT
+// });
 
-var users = {
+const users = {
   leo: {
     name: 'leo',
     password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
@@ -27,246 +27,223 @@ var users = {
   }
 };
 
-var validate = function(request, username, password, callback) {
-  var user = users[username];
-  if (!user) {
-    return callback(null, false);
-  }
-
-  Bcrypt.compare(password, user.password, (err, isValid) => {
-      callback(err, isValid, { id: user.id, name: user.name });
-  });
-};
-
-// TODO: clean up
-
 const LARGE_RESPONSE = JSON.stringify({
   data: new Array(1024 * 1024 * 10).join('0')
 });
 
-server.route({
-  method: 'GET',
-  path: '/largeResponse',
-  handler: function largeResponse(req, reply) {
-    return reply(LARGE_RESPONSE);
+const validate = async (request, username, password, h) => {
+  const user = users[username];
+  if (!user) {
+    return { credentials: null, isValid: false };
   }
-});
 
-server.route({
-  method: 'POST',
-  path: '/setscookie',
-  handler: setsCookie
-});
+  const isValid = await Bcrypt.compare(password, user.password);
+  const credentials = { id: user.id, name: user.name };
+  return { isValid, credentials };
+};
 
-server.route({
-  method: 'GET',
-  path: '/expectscookie',
-  handler: expectsCookie,
-  config: {
-    state: {
-      parse: true,
-      failAction: 'log'
-    }
-  }
-});
+const main = async () => {
+  const server = Hapi.server({ port: PORT });
+  await server.register(require('hapi-auth-basic'));
+  server.auth.strategy('simple', 'basic', { validate });
 
-server.route({
-  method: 'GET',
-  path: '/pets',
-  handler: index
-});
-server.route({
-  method: 'POST',
-  path: '/pets',
-  handler: create
-});
-server.route({
-  method: 'GET',
-  path: '/pets/{id}',
-  handler: read
-});
-server.route({
-  method: 'GET',
-  path: '/_stats',
-  handler: stats
-});
-
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: index
-});
-
-server.route({
-  method: 'POST',
-  path: '/',
-  handler: postIndex
-});
-
-server.route({
-  method: 'GET',
-  path: '/header',
-  handler: function(request, reply) {
-    return reply().header('x-auth', 'secret');
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/expectsHeader',
-  handler: function(request, reply) {
-    if (request.headers['x-auth'] && request.headers['x-auth'] === 'secret') {
-      return reply({success: true}).code(200);
-    } else {
-      return reply().code(403);
-    }
-  }
-});
-
-server.route(
-  {
+  //
+  // routes
+  //
+  server.route({
     method: 'GET',
-    path: '/journeys',
-    handler: getJourneys
+    path: '/protected',
+    config: {
+      auth: 'simple',
+      handler: function(req, h) {
+        return 'secret timestamp for ' + req.auth.credentials.name + ': ' + Date.now();
+      }
+    }
   });
 
-server.route({
+  route(server);
+
+  server.state('testCookie', {
+    ttl: null,
+    isSecure: false,
+    isHttpOnly: true,
+    encoding: 'base64json',
+    clearInvalid: false,
+    strictHeader: true
+  });
+
+  await server.start();
+  return server;
+};
+
+// TODO: clean up
+
+function route(server) {
+  server.route({
+    method: 'GET',
+    path: '/largeResponse',
+    handler: function largeResponse(req, h) {
+      return LARGE_RESPONSE;
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/setscookie',
+    handler: setsCookie
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/expectscookie',
+    handler: expectsCookie,
+    config: {
+      state: {
+        parse: true,
+        failAction: 'log'
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/pets',
+    handler: index
+  });
+  server.route({
+    method: 'POST',
+    path: '/pets',
+    handler: create
+  });
+  server.route({
+    method: 'GET',
+    path: '/pets/{id}',
+    handler: read
+  });
+  server.route({
+    method: 'GET',
+    path: '/_stats',
+    handler: stats
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: index
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/',
+    handler: postIndex
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/header',
+    handler: function(request, h) {
+      return h.response().header('x-auth', 'secret');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/expectsHeader',
+    handler: function(request, h) {
+      if (request.headers['x-auth'] && request.headers['x-auth'] === 'secret') {
+        return {success: true};
+      } else {
+        return h.response().code(403);
+      }
+    }
+  });
+
+  server.route(
+    {
+      method: 'GET',
+      path: '/journeys',
+      handler: getJourneys
+    });
+
+  server.route({
     method: 'GET',
     path: '/journey/{id}',
     handler: getJourney
   });
 
-server.route({
-  method: 'GET',
-  path: '/devices',
-  handler: getDevices
-});
-
-server.route({
-  method: 'PUT',
-  path: '/devices/{id}',
-  handler: putDevice
-});
-
-//
-// Used by loop_range.json test
-//
-server.route([
-  {
+  server.route({
     method: 'GET',
-    path: '/loop/9',
-    handler: ok
-  },
-  {
-    method: 'GET',
-    path: '/loop/10',
-    handler: ok
-  },
-  {
-    method: 'GET',
-    path: '/loop/11',
-    handler: ok
-  }
-]);
+    path: '/devices',
+    handler: getDevices
+  });
 
-function ok(req, reply) {
-  reply('ok');
+  server.route({
+    method: 'PUT',
+    path: '/devices/{id}',
+    handler: putDevice
+  });
+
+  //
+  // Used by loop_range.json test
+  //
+  server.route([
+    {
+      method: 'GET',
+      path: '/loop/9',
+      handler: ok
+    },
+    {
+      method: 'GET',
+      path: '/loop/10',
+      handler: ok
+    },
+    {
+      method: 'GET',
+      path: '/loop/11',
+      handler: ok
+    }
+  ]);
 }
 
-server.state('testCookie', {
-  ttl: null,
-  isSecure: false,
-  isHttpOnly: true,
-  encoding: 'base64json',
-  clearInvalid: false,
-  strictHeader: true
-});
+function ok(req, h) {
+  return 'ok';
+}
 
 var DB = {};
 
 let reporters = [];
-if (!process.env.SILENT) {
-  reporters.push({
-    reporter: require('good-console'),
-    events: {error: '*', log: '*', response: '*'}
-  });
+
+function index(req, h) {
+  return 'ok';
 }
 
-server.register([{
-  register: require('good'),
-  options: {
-    opsInterval: 1000,
-    reporters: reporters
-  }
-},
-{
-  register: Basic
-}], function(err) {
-  if (err) {
-    console.error(err);
-  } else {
-    server.auth.strategy('simple', 'basic', { validateFunc: validate });
-
-    server.route({
-      method: 'GET',
-      path: '/protected',
-      config: {
-        auth: 'simple',
-        handler: function(request, reply) {
-          console.log(request.auth);
-          return reply('secret timestamp for ' + request.auth.credentials.name + ': ' + Date.now());
-        }
-      }
-    });
-
-    server.start(function() {
-      console.log('Target listening on 0.0.0.0:' + PORT);
-
-      setInterval(function() {
-        console.log(new Date());
-        console.log('REQUEST_COUNT = %s', REQUEST_COUNT);
-      }, 20 * 1000);
-    });
-  }
-});
-
-function index(req, reply) {
-  //req.log(['index']);
-  setTimeout(function() {
-    REQUEST_COUNT++;
-    reply('ok');
-  }, Math.floor(Math.random() * 100));
-}
-
-function postIndex(req, reply) {
+function postIndex(req, h) {
   reply('ok');
 }
 
-function create(req, reply) {
-  var id = uuid.v4();//.split('-')[0];
+function create(req, h) {
+  var id = uuid.v4();
   DB[id] = req.payload;
   DB[id].id = id;
   REQUEST_COUNT++;
-  return reply({id: id}).code(201);
+  return h.response({id: id}).code(201);
 }
 
-function read(req, reply) {
+function read(req, h) {
   REQUEST_COUNT++;
   var result = DB[req.params.id];
-  //console.log(typeof result);
-  //console.log(result);
   if (result) {
-    return reply(result).code(200);
+    return h.response(result).code(200);
   } else {
-    return reply().code(404);
+    return h.response().code(404);
   }
 }
 
-function stats(req, reply) {
-  return reply({
+function stats(req, h) {
+  return {
     requestCount: REQUEST_COUNT,
     cookies: COOKIES
-  });
+  };
 }
 
 //
@@ -274,13 +251,14 @@ function stats(req, reply) {
 // curl -v 0.0.0.0:3003/expectscookie -b 'testCookie=eyJ1aWQiOiIxNWMwMjNkMC02YmMxLTRkODEtYmQ1OS0wNjRmYjhmMGU0YTkifQ==;'
 //
 
-function setsCookie(req, reply) {
+function setsCookie(req, h) {
   var newuid = uuid.v4();
-  console.log('setting testCookie.uid to %j', newuid);
-  reply('ok').state('testCookie', {uid: newuid});
+  // console.log('setting testCookie.uid to %j', newuid);
+  h.state('testCookie', {uid: newuid});
+  return h.continue;
 }
 
-function expectsCookie(req, reply) {
+function expectsCookie(req, h) {
   console.log('req.state = %j', req.state);
   //console.log('req.state.testCookie = %j', req.state.testCookie);
   if (req.state.testCookie) {
@@ -289,13 +267,13 @@ function expectsCookie(req, reply) {
     } else {
       COOKIES[req.state.testCookie.uid] = 1;
     }
-    return reply('ok');
+    return 'ok';
   } else {
-    return reply().code(403);
+    return h.response().code(403);
   }
 }
 
-function getJourneys(req, reply) {
+function getJourneys(req, h) {
   var response = `
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xmlns:tns1="http://" xmlns:tns="http://">
   <soap:Header></soap:Header>
@@ -319,11 +297,11 @@ function getJourneys(req, reply) {
     </tns1:GetJourneys>
   </soap:Body>
 </soap:Envelope>`;
-  return reply(response)
+  return h.response(response)
     .type('application/xml');
 }
 
-function getJourney(req, reply) {
+function getJourney(req, h) {
   console.log(req.params.id);
   if (req.params.id === '1') {
     let response = `
@@ -341,13 +319,13 @@ function getJourney(req, reply) {
     </tns1:GetJourney>
   </soap:Body>
 </soap:Envelope>`;
-    return reply(response).type('application/xml');
+    return h.response(response).type('application/xml');
   }
 
-  return reply('').code(404);
+  return h.response('').code(404);
 }
 
-function getDevices(req, reply) {
+function getDevices(req, h) {
   var response = `
 [
   {
@@ -378,18 +356,25 @@ function getDevices(req, reply) {
   }
 ]
 `;
-  return reply(response)
+  return h.response(response)
     .type('application/json')
     .code(200);
 }
 
-function putDevice(req, reply) {
+function putDevice(req, h) {
   if (req.params.id === "4dcb754442b1285785b81833c77f4a46" || req.params.id === "e87c45241a484a3db9730ae4b98678d4") {
-    return reply('{"status": "ok"}')
+    return h.response('{"status": "ok"}')
       .type('application/json')
       .code(200);
   } else {
-    return reply('')
+    return h.response('')
       .code(404);
   }
 }
+
+main()
+  .then((server) => console.log(`Server listening on ${server.info.uri}`))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
