3a52eff2bfca19b3c5c144df83075e9923572274
hveldstra <h@veldstra.org>
2021-03-02 17:10:55 +0000

    fix(ci): use bash

--- .circleci/npm-publish.sh
+++ .circleci/npm-publish.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
 set -eu -o pipefail
 
 if [ -z ${CIRCLE_TAG:-""} ] ; then
+    echo "No tag, not doing anything"
     exit 0
 fi
 
 if [ $CIRCLE_BRANCH != "master" ] ; then
+    echo "Not on main branch, not doing anything"
     exit 0
 fi
 
