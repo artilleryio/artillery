0c71b35755ceb40eeb8df4a8824a280f70249076
Dennis Martinez <dennis@dennmart.com>
2021-04-19 16:38:51 +0900

    ci: update npm publish script
    - Remove check for CIRCLE_TAG since this script will only get executed
    when a Git tag is pushed.
    
    - Remove check for CIRCLE_BRANCH since tagged pushes won't have this
    information.
    
    - If the pushed tag is a dev release (the tag does contains '-dev'),
    publish the package with the 'dev' dist-tag. Otherwise, publish as
    the latest release

--- .circleci/npm-publish.sh
+++ .circleci/npm-publish.sh
@@ -2,16 +2,11 @@
 
 set -eu -o pipefail
 
-if [ -z ${CIRCLE_TAG:-""} ] ; then
-    echo "No tag, not doing anything"
-    exit 0
+echo "Publishing package on npm for $CIRCLE_TAG"
+
+if [[ ${CIRCLE_TAG} == *"-dev"* ]] ; then
+    npm publish --tag dev
+else
+    echo "npm publish"
+    npm publish
 fi
-
-if [ $CIRCLE_BRANCH != "master" ] ; then
-    echo "Not on main branch, not doing anything"
-    exit 0
-fi
-
-echo "Publishing package on npm for $CIRCLE_TAG on $CIRCLE_BRANCH"
-
-npm publish
