4cc3d91efda8658f726fe045035318621d47a8cc
hveldstra <h@veldstra.org>
2021-04-21 08:48:39 +0100

    chore: allow sentence case in commit subjects

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,21 @@
 const config = require('@commitlint/config-conventional');
 
 const types = config.rules['type-enum'][2].concat(['dep']);
+const cases = [
+  'lower-case', // default
+  'upper-case', // UPPERCASE
+  'camel-case', // camelCase
+  'kebab-case', // kebab-case
+  'pascal-case', // PascalCase
+  'sentence-case', // Sentence case
+  'snake-case', // snake_case
+  'start-case' // Start Case
+];
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', types]
+    'type-enum': [2, 'always', types],
+    'subject-case': [2, 'always', cases ]
   }
 };
