619b91fa630aa669a550a2755baaf95ffcb3c46a
Dominic Watson <watson.dominic@gmail.com>
2021-06-08 13:39:37 +0100

    #1065 add ability to use hooks with socketio scenarios

--- core/lib/engine_socketio.js
+++ core/lib/engine_socketio.js
@@ -31,6 +31,33 @@ SocketIoEngine.prototype.createScenario = function(scenarioSpec, ee) {
   // Adds scenario overridden configuration into the static config
   this.socketioOpts = {...this.socketioOpts, ...scenarioSpec.socketio}
 
+  ensurePropertyIsAList(scenarioSpec, 'beforeRequest');
+  ensurePropertyIsAList(scenarioSpec, 'afterResponse');
+  ensurePropertyIsAList(scenarioSpec, 'beforeScenario');
+  ensurePropertyIsAList(scenarioSpec, 'afterScenario');
+  ensurePropertyIsAList(scenarioSpec, 'onError');
+
+  // Add scenario-level hooks if needed:
+  // For now, just turn them into function steps and insert them
+  // directly into the flow array.
+  // TODO: Scenario-level hooks will probably want access to the
+  // entire scenario spec rather than just the userContext.
+  const beforeScenarioFns = _.map(
+    scenarioSpec.beforeScenario,
+    function(hookFunctionName) {
+      return {'function': hookFunctionName};
+    });
+  const afterScenarioFns = _.map(
+    scenarioSpec.afterScenario,
+    function(hookFunctionName) {
+      return {'function': hookFunctionName};
+    });
+
+  const newFlow = beforeScenarioFns.concat(
+    scenarioSpec.flow.concat(afterScenarioFns));
+
+  scenarioSpec.flow = newFlow;
+
   let tasks = _.map(scenarioSpec.flow, function(rs) {
     if (rs.think) {
       return engineUtil.createThink(rs, _.get(self.config, 'defaults.think', {}));
