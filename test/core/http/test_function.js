8e14324edbbf41be537da2a0f48be559f660f8b1
hveldstra <h@veldstra.org>
2021-08-11 23:25:04 +0100

    test: fix function step tests

--- test/core/http/test_function.js
+++ test/core/http/test_function.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const test = require('tape');
-const createRunner = require('../../../lib/runner-sp');
+const test = require('tap').test;
+const createRunner = require('../../../lib/launch-local');
+const sleep = require('../../../lib/util/sleep');
 const path = require('path');
 
-test('Should execute a simple function', (t) => {
-  startRunner('simple_function.json', {}, t, (stats) => {
-    t.deepEquals(
+test('Should execute a simple function', async (t) => {
+  await runOnDone('simple_function.json', {}, async (stats) => {
+    t.same(
       Object.keys(stats.counters).filter((counter) =>
         counter.startsWith('errors.')
       ),
       [],
       'No errors'
     );
-    t.equals(
+    t.equal(
       stats.counters['simpleFunction'],
       1,
       'Function "simpleFunction" called once'
@@ -21,21 +22,21 @@ test('Should execute a simple function', (t) => {
   });
 });
 
-test('Should await async function', (t) => {
-  startRunner('async_function.json', {}, t, (stats) => {
-    t.deepEquals(
+test('Should await async function', async (t) => {
+  await runOnDone('async_function.json', {}, async (stats) => {
+    t.same(
       Object.keys(stats.counters).filter((counter) =>
         counter.startsWith('errors.')
       ),
       [],
       'No errors'
     );
-    t.equals(
+    t.equal(
       stats.counters['asyncFunctionOrder'],
       1,
       'Function "asyncFunction" finished first'
     );
-    t.equals(
+    t.equal(
       stats.counters['otherFunctionOrder'],
       2,
       'Function "otherFunction" finished second'
@@ -43,9 +44,9 @@ test('Should await async function', (t) => {
   });
 });
 
-test('Should emit error if function does not exist', (t) => {
-  startRunner('undefined_function.json', {}, t, (stats) => {
-    t.equals(
+test('Should emit error if function does not exist', async (t) => {
+  await runOnDone('undefined_function.json', {}, async (stats) => {
+    t.equal(
       stats.counters['errors.Undefined function "undefinedFunction"'],
       1,
       'Undefined function error count'
@@ -53,15 +54,15 @@ test('Should emit error if function does not exist', (t) => {
   });
 });
 
-test('Should emit error with "code" argument if function calls next({ code: 123 })', (t) => {
-  startRunner('error_code_function.json', {}, t, (stats) => {
-    t.equals(stats.counters['errors.123'], 1, 'Function finished with error');
+test('Should emit error with "code" argument if function calls next({ code: 123 })', async (t) => {
+  await runOnDone('error_code_function.json', {}, async (stats) => {
+    t.equal(stats.counters['errors.123'], 1, 'Function finished with error');
   });
 });
 
-test('Should emit error with "message" argument if function calls next({ message: "AwesomeErrorMessage" })', (t) => {
-  startRunner('error_message_function.json', {}, t, (stats) => {
-    t.equals(
+test('Should emit error with "message" argument if function calls next({ message: "AwesomeErrorMessage" })', async (t) => {
+  await runOnDone('error_message_function.json', {}, async (stats) => {
+    t.equal(
       stats.counters['errors.AwesomeErrorMessage'],
       1,
       'Function finished with error'
@@ -69,17 +70,20 @@ test('Should emit error with "message" argument if function calls next({ message
   });
 });
 
-function startRunner(scriptName, payload, t, doneCallback) {
+
+async function runOnDone(scriptName, payload, fn) {
   const scriptPath = path.join(__dirname, 'scripts', scriptName);
   const script = require(scriptPath);
   const opts = { script, scriptPath };
 
-  const runner = createRunner(script, payload, opts);
-  const ee = runner.events;
-  ee.on('done', (stats) => {
-    doneCallback(stats);
-    runner.shutdown(() => t.end());
-  });
+  const runner = await createRunner(script, payload, opts);
+  let done = false;
 
+  runner.events.once('done', async (stats) => {
+    await fn(stats);
+    await runner.shutdown();
+    done = true;
+  });
   runner.run();
+  while(!done) { await sleep(1000); }
 }
