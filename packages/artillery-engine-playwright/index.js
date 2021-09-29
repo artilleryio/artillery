15782998bffab481aa0628a6bfbf27d157653c05
hveldstra <h@veldstra.org>
2021-09-30 00:03:59 +0100

    init

---
+++ packages/artillery-engine-playwright/index.js
@@ -0,0 +1,80 @@
+const debug = require('debug')('engine:playwright');
+const P = require('playwright');
+const chromium = P.chromium;
+
+class PlaywrightEngine {
+  constructor(script) {
+    debug('constructor');
+    this.config = script.config;
+    return this;
+  }
+
+  createScenario(spec, events) {
+    debug('createScenario');
+    debug(spec);
+
+    const self = this;
+    return async function scenario(initialContext, cb) {
+      events.emit('started');
+      const browser = await chromium.launch({
+          headless: self.config.engines.playwright.headless || true
+        });
+      debug('browser created');
+      const context = await browser.newContext();
+      debug('context created');
+      const uniquePageLoadToTiming = {};
+      try {
+        const page = await context.newPage();
+        debug('page created');
+
+        page.on('domcontentloaded', async (page) => {
+          const performanceTimingJson = await page.evaluate(() => JSON.stringify(window.performance.timing));
+          const performanceTiming = JSON.parse(performanceTimingJson);
+
+          if(uniquePageLoadToTiming[page.url()+performanceTiming.connectStart]) {
+            return;
+          } else {
+            uniquePageLoadToTiming[page.url()+performanceTiming.connectStart] = performanceTiming;
+          }
+
+          debug('domcontentloaded:', page.url());
+;
+          const startToInteractive = performanceTiming.domInteractive - performanceTiming.navigationStart;
+
+          events.emit('counter', 'engine.playwright.page_domcontentloaded', 1);
+          events.emit('counter', `engine.playwright.page_domcontentloaded.${page.url()}`)
+          events.emit('histogram', 'engine.playwright.page_domcontentloaded.dominteractive', startToInteractive);
+          events.emit('histogram', `engine.playwright.page_domcontentloaded.dominteractive.${page.url()}`, startToInteractive);
+        });
+        page.on('load', (page) => {
+          debug('load:', page.url());
+        });
+        page.on('pageerror', (error) => {
+          debug('pageerror:', page.url());
+        });
+        page.on('requestfinished', (request) => {
+          const timing = request.timing();
+          events.emit('histogram', 'engine.playwright.response_time',timing.responseEnd - timing.responseStart);
+        });
+        page.on('response', (response) => {
+        });
+
+        const fn = self.config.processor[spec.flowFunction];
+        await fn(page);
+        if(cb) { cb(null, initialContext); }
+        return initialContext;
+      } catch(err) {
+        if(cb) {
+          cb(err, initialContext);
+        } else {
+          throw err;
+        }
+      } finally {
+        await context.close();
+        await browser.close();
+      }
+    }
+  }
+}
+
+module.exports = PlaywrightEngine;
