8d3afe5d39256e7270ca9e8322362255847598b4
hveldstra <h@veldstra.org>
2021-12-21 19:21:42 +0000

    2.0.0-9

--- core/lib/engine_util.js
+++ core/lib/engine_util.js
@@ -80,7 +80,7 @@ function createLoopWithCount(count, steps, opts) {
         overValues = opts.overValues;
         loopValue = overValues[i];
       } else if (opts.overValues && typeof opts.overValues === 'string') {
-        overValues = context.vars[opts.overValues];
+        overValues = _.get(context.vars, opts.overValues);
         if (L.isArray(overValues)) {
           loopValue = overValues[i];
         } else {
