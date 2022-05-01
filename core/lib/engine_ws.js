b60863e818d64f54fd9882e4bbe01fabc470ae9b
Scott Deakin <sdeaks@gmail.com>
2022-05-01 10:13:36 +0100

    fix: allow empty payload in ws send

--- core/lib/engine_ws.js
+++ core/lib/engine_ws.js
@@ -47,6 +47,8 @@ function getMessageHandler(context, params, ee, timeout, callback) {
   }, timeout * 1000);
 
   return function messageHandler(event) {
+    ee.emit('counter', 'websocket.messages_received', 1);
+    ee.emit('rate', 'websocket.receive_rate');
     done = true;
     const { data } = event;
 
@@ -164,9 +166,7 @@ WSEngine.prototype.step = function (requestSpec, ee) {
   }
 
   const f = function (context, callback) {
-    ee.emit('counter', 'websocket.messages_sent', 1);
-    ee.emit('rate', 'websocket.send_rate');
-    const params = requestSpec.send || requestSpec.wait;
+    const params = requestSpec.wait || requestSpec.send;
 
     // match exists on a string, so check it's not one first
     let captureOrMatch =
@@ -189,15 +189,18 @@ WSEngine.prototype.step = function (requestSpec, ee) {
     }
 
     // Backwards compatible with previous version of `send` api
-    let payload = template(captureOrMatch ? params.payload : params, context);
+    let payload = captureOrMatch ? params.payload : params;
 
-    if (payload) {
+    if (payload !== undefined) {
+      payload = template(payload, context);
       if (typeof payload === 'object') {
         payload = JSON.stringify(payload);
       } else {
-        payload = payload.toString();
+        payload = _.toString(payload);
       }
 
+      ee.emit('counter', 'websocket.messages_sent', 1);
+      ee.emit('rate', 'websocket.send_rate');
       debug('WS send: %s', payload);
 
       context.ws.send(payload, function (err) {
--- test/core/test_ws_match_wait.js
+++ test/core/test_ws_match_wait.js
@@ -79,7 +79,9 @@ test('should wait for a websocket response without send', (t) => {
             send: {
               payload: 'should wait for a websocket response without send',
               match: { json: '$.bar', value: 'foo' }
-            },
+            }
+          },
+          {
             wait: {
               match: { json: '$.bar', value: 'baz' }
             }
@@ -94,6 +96,11 @@ test('should wait for a websocket response without send', (t) => {
       let c = report.counters;
       t.equal(c['vusers.failed'], 0, 'There should be no failures');
       t.equal(c['websocket.messages_sent'], 1, 'All messages should be sent');
+      t.equal(
+        c['websocket.messages_received'],
+        2,
+        'All messages should be received'
+      );
 
       ee.stop().then(() => {
         targetServer.close(t.end);
@@ -243,6 +250,138 @@ test('should use config.timeout on capture', (t) => {
       let c = report.counters;
       t.equal(c['vusers.failed'], 1, 'There should be one failure');
       t.equal(c['websocket.messages_sent'], 1, 'All messages should be sent');
+      ee.stop().then(() => {
+        targetServer.close(t.end);
+      });
+    });
+
+    ee.run();
+  });
+});
+
+test('should match allow an empty payload to be sent', (t) => {
+  const server = http.createServer();
+  const wss = new WebSocket.Server({ server: server });
+  const targetServer = server.listen(0);
+
+  wss.on('connection', function (ws) {
+    ws.on('message', function () {
+      ws.send(JSON.stringify({ bar: 'baz' }));
+    });
+  });
+
+  const script = {
+    config: {
+      target: `ws://127.0.0.1:${targetServer.address().port}`,
+      phases: [{ duration: 1, arrivalCount: 1 }]
+    },
+    scenarios: [
+      {
+        engine: 'ws',
+        flow: [
+          {
+            send: {
+              payload: '',
+              match: { json: '$.bar', value: 'baz' }
+            }
+          }
+        ]
+      }
+    ]
+  };
+
+  runner(script).then(function (ee) {
+    ee.on('done', (report) => {
+      let c = report.counters;
+      t.equal(c['vusers.failed'], 0, 'There should be no failures');
+      t.equal(c['websocket.messages_sent'], 1, 'All messages should be sent');
+
+      ee.stop().then(() => {
+        targetServer.close(t.end);
+      });
+    });
+
+    ee.run();
+  });
+});
+
+test('should match allow a simple empty string to be sent', (t) => {
+  const server = http.createServer();
+  const wss = new WebSocket.Server({ server: server });
+  const targetServer = server.listen(0);
+
+  wss.on('connection', function (ws) {
+    ws.on('message', function () {
+      ws.send(JSON.stringify({ bar: 'baz' }));
+    });
+  });
+
+  const script = {
+    config: {
+      target: `ws://127.0.0.1:${targetServer.address().port}`,
+      phases: [{ duration: 1, arrivalCount: 1 }]
+    },
+    scenarios: [
+      {
+        engine: 'ws',
+        flow: [
+          {
+            send: ''
+          }
+        ]
+      }
+    ]
+  };
+
+  runner(script).then(function (ee) {
+    ee.on('done', (report) => {
+      let c = report.counters;
+      t.equal(c['vusers.failed'], 0, 'There should be no failures');
+      t.equal(c['websocket.messages_sent'], 1, 'All messages should be sent');
+
+      ee.stop().then(() => {
+        targetServer.close(t.end);
+      });
+    });
+
+    ee.run();
+  });
+});
+
+test('should match allow an undefined variable to be sent', (t) => {
+  const server = http.createServer();
+  const wss = new WebSocket.Server({ server: server });
+  const targetServer = server.listen(0);
+
+  wss.on('connection', function (ws) {
+    ws.on('message', function () {
+      ws.send(JSON.stringify({ baz: 'foo' }));
+    });
+  });
+
+  const script = {
+    config: {
+      target: `ws://127.0.0.1:${targetServer.address().port}`,
+      phases: [{ duration: 1, arrivalCount: 1 }]
+    },
+    scenarios: [
+      {
+        engine: 'ws',
+        flow: [
+          {
+            send: '{{ nothing }}'
+          }
+        ]
+      }
+    ]
+  };
+
+  runner(script).then(function (ee) {
+    ee.on('done', (report) => {
+      let c = report.counters;
+      t.equal(c['vusers.failed'], 0, 'There should be no failures');
+      t.equal(c['websocket.messages_sent'], 1, 'All messages should be sent');
+
       ee.stop().then(() => {
         targetServer.close(t.end);
       });
