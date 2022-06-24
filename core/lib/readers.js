a33ad60da35a278dcf0d44ca0f9e7fcc958f981d
Hassy Veldstra <h@veldstra.org>
2022-06-24 14:01:40 +0100

    feat: add ability to make all rows in a CSV payload available to each VU (#1485)
    * feat: add ability to make all rows in a CSV payload available to each VU
    
    Setting the "loadAll" and "name" properties on a CSV payload will make all rows
    available to the VU, under the variable with that name. The type of the value is
    an array of objects, where each object has properties corresponding to
    the "fields" spec of the CSV payload.
    
    The array of rows as-is will be returned if "fields" spec is not provided

--- core/lib/readers.js
+++ core/lib/readers.js
@@ -8,11 +8,14 @@ const _ = require('lodash');
 
 module.exports = createReader;
 
-function createReader(order) {
+function createReader(order, spec) {
   if (order === 'sequence') {
     return createSequencedReader();
+  } else if (typeof order === 'undefined' && (typeof spec?.name !== 'undefined') && spec?.loadAll === true) {
+    return createEverythingReader(spec);
+  } else { // random
+    return createRandomReader();
   }
-  return createRandomReader();
 }
 
 function createSequencedReader() {
@@ -25,6 +28,33 @@ function createSequencedReader() {
       i = 0;
     }
     return result;
+  };
+}
+
+function createEverythingReader(spec) {
+  let parsedData;
+
+  return function (data) {
+    if (!parsedData) {
+      parsedData = [];
+
+      // Parse the row into an object based on the fields spec
+      if (spec.fields?.length > 0) {
+        for (const row of data) {
+          let o = {};
+            for(let i = 0; i < spec.fields.length; i++) {
+              const fieldName = spec.fields[i];
+              o[fieldName] = row[i];
+            }
+          parsedData.push(o);
+        }
+      } else {
+        // Otherwise just return the array of rows
+        parsedData = data;
+      }
+    }
+
+    return parsedData;
   };
 }
 
