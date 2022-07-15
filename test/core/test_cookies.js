afccde0c6218f54493be6fb27337a0bb212d7391
juanig1 <jgil@artillery.io>
2022-07-15 13:59:56 -0300

    fix: add test where cookie is set in a processor

--- test/core/test_cookies.js
+++ test/core/test_cookies.js
@@ -114,22 +114,3 @@ test('no default cookie still sends cookies defined in script', function (t) {
     ee.run();
   });
 });
-
-test('no default cookie still sends cookies defined in custom JS', function (t) {
-  var script = require('./scripts/test-set-cookie-js.json');
-  var js = require('./scripts/test-set-cookie-js.js');
-  runner(script).then(function (ee) {
-    ee.on('done', function (nr) {
-      const report = SSMS.legacyReport(nr).report();
-      t.ok(
-        report.codes[200] && report.codes[200] > 0,
-        'There should be some 200s'
-      );
-      t.ok(report.codes[403] === undefined, 'There should be no 403s');
-      ee.stop().then(() => {
-        t.end();
-      });
-    });
-    ee.run();
-  });
-});
--- test/core/unit/engine_http.test.js
+++ test/core/unit/engine_http.test.js
@@ -302,6 +302,58 @@ test('HTTP engine', function (tap) {
     });
   });
 
+  tap.test('custom cookie js', function (t) {
+    const cookie = 'something';
+    const cookieValue = 'abcde';
+    const target = nock('http://localhost:8888')
+      .get('/')
+      .reply(200, function () {
+        t.equal(
+          this.req.cookie[cookie],
+          cookieValue,
+          'Cookie not found. Should be set in processor logic'
+        );
+
+        return 'ok';
+      });
+
+    const script = {
+      config: {
+        target: 'http://localhost:8888'
+      },
+      processor: {
+        setCookie: function(requestParams, context, ee, next) {
+        requestParams.cookie = { cookie : "FAILING TEST" };
+          return next();
+        },
+      scenarios: [
+        {
+          flow: [
+            {
+              get: {
+                url: '/',
+                beforeRequest: "setCookie"
+              }
+            }
+          ]
+        }
+      ]
+    };
+
+    const engine = new HttpEngine(script);
+    const ee = new EventEmitter();
+    const runScenario = engine.createScenario(script.scenarios[0], ee);
+
+    runScenario({ vars: {} }, function userDone(err) {
+      if (err) {
+        t.fail();
+      }
+
+      t.ok(target.isDone(), 'Should have made a request to /');
+
+      t.end();
+    });
+  });
   tap.test('url and uri parameters', function (t) {
     const target = nock('http://localhost:8888')
       .get('/hello?hello=world')
