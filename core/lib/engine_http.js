96e0cc1e6baef2329edc30580184a7d89c535519
juanig1 <jgil@artillery.io>
2022-07-14 21:35:53 -0300

    fix: update cookies on requestParams to address setting them in js

--- core/lib/engine_http.js
+++ core/lib/engine_http.js
@@ -268,7 +268,7 @@ HttpEngine.prototype.step = function step(requestSpec, ee, opts) {
       timeout: timeout * 1000
     });
 
-    if (context._enableCookieJar || typeof (requestParams.cookie) === 'object') {
+    if (context._enableCookieJar || typeof (params.cookie) === 'object') {
       requestParams.cookieJar = context._jar;
     }
 
@@ -398,13 +398,14 @@ HttpEngine.prototype.step = function step(requestSpec, ee, opts) {
         requestParams.headers = templatedHeaders;
 
         if (
-          typeof params.cookie === 'object' ||
+          typeof requestParams.cookie === 'object' ||
           typeof context._defaultCookie === 'object'
         ) {
+          requestParams.cookieJar = context._jar;
           const cookie = Object.assign(
             {},
             context._defaultCookie,
-            params.cookie
+            requestParams.cookie
           );
           Object.keys(cookie).forEach(function (k) {
             context._jar.setCookieSync(
--- test/core/scripts/test-set-cookie-js.js
+++ test/core/scripts/test-set-cookie-js.js
@@ -3,7 +3,6 @@ module.exports = {
 };
 
 function setCookie(requestParams, context, ee, next) {
-  requestParams.cookie = { "testCookie":
-"eyJ1aWQiOiIxNWMwMjNkMC02YmMxLTRkODEtYmQ1OS0wNjRmYjhmMGU0YTkifQ==;" };
+  requestParams.cookie = { "testCookie": "eyJ1aWQiOiIxNWMwMjNkMC02YmMxLTRkODEtYmQ1OS0wNjRmYjhmMGU0YTkifQ==;" };
   return next();
 }
