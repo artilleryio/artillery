5e4180124e90f8e7aa9f76d977bf7ca29db16c55
hveldstra <h@veldstra.org>
2022-11-17 21:47:26 +0000

    refactor: build global object incrementally

--- packages/artillery/lib/artillery-global.js
+++ packages/artillery/lib/artillery-global.js
@@ -1,84 +1,47 @@
 const version = require('../package.json').version;
-const chalk = require('chalk');
 
 const createReporter = require('./console-reporter');
 const util = require('./util');
-const { SSMS } = require('core').ssms;
-const EventEmitter = require('events');
 const telemetry = require('./telemetry').init();
 
+const { updateGlobalObject } = require('core');
+
 async function createGlobalObject(opts) {
-  if (typeof global.artillery === 'object') {
-    return;
-  }
+  global.artillery = global.artillery || {};
+  global.artillery.version = version;
+  global.artillery.runtimeOptions = global.artillery.runtimeOptions || {};
+  global.artillery.runtimeOptions.legacyReporting =
+    typeof process.env.ARTILLERY_USE_LEGACY_REPORT_FORMAT !== 'undefined';
+  global.artillery._workerThreadSend = global.artillery._workerThreadSend || null;
 
-  global.artillery = {
-    version: version,
-    runtimeOptions: {
-      legacyReporting:
-        typeof process.env.ARTILLERY_USE_LEGACY_REPORT_FORMAT !== 'undefined',
-      extendedHTTPMetrics:
-        typeof process.env.ARTILLERY_EXTENDED_HTTP_METRICS !== 'undefined'
-    },
-    metrics: {
-      event: async function (msg, opts) {
-        if (opts.level === 'error') {
-          console.log(chalk.red(msg));
-        } else {
-          console.log(msg);
-        }
+  global.artillery._exitCode = 0;
+
+  global.artillery.telemetry = global.artillery.telemetry || telemetry;
+
+  global.artillery.logger = global.artillery.logger || function (opts) {
+    return {
+      log: (...args) => {
+        global.artillery.globalEvents.emit('log', opts, ...args);
       }
-    },
-
-    util: {
-      template: require('../util').template
-    },
-
-    plugins: [],
-
-    extensionEvents: [],
-    ext: async function (event) {
-      // TODO: Validate events object
-      this.extensionEvents.push(event);
-    },
-
-    _workerThreadSend: null,
-    _exitCode: 0,
-    get suggestedExitCode() {
-      return this._exitCode;
-    },
-    set suggestedExitCode(code) {
-      this._exitCode = code;
-      if (typeof this._workerThreadSend === 'function') {
-        this._workerThreadSend({ event: 'setSuggestedExitCode', code: code });
-      }
-    },
-
-    logger: function (opts) {
-      return {
-        log: (...args) => {
-          global.artillery.globalEvents.emit('log', opts, ...args);
-        }
-      };
-    },
-
-    log: function (...args) {
-      global.artillery.globalEvents.emit('log', {}, ...args);
-    },
-
-    shutdown: async function () {
-      // TODO: Move graceful shutdown logic into here
-      process.exit(artillery.suggestedExitCode);
-    }
+    };
   };
 
-  global.artillery.telemetry = telemetry;
-  global.artillery.globalEvents = new EventEmitter();
+  global.artillery.log = global.artillery.log || function (...args) {
+    global.artillery.globalEvents.emit('log', {}, ...args);
+  };
+
+  global.artillery.shutdown = global.artillery.shutdown || async function () {
+    // TODO: Move graceful shutdown logic into here
+    process.exit(global.artillery.suggestedExitCode);
+  };
+
+  //global.artillery.suggestedExitCode = 99;
 
   // TODO: Refactor
   global.artillery.__createReporter = createReporter;
   global.artillery.__util = util;
-  global.artillery.__SSMS = SSMS;
+
+  await updateGlobalObject();
 }
 
 module.exports = {
--- packages/core/index.js
+++ packages/core/index.js
@@ -1,15 +1,68 @@
-// const { createGlobalObject } = require('../lib/artillery-global');
+const EventEmitter = require('events');
+const chalk = require('chalk');
 
-// async function main() {
-//   await createGlobalObject();
-// }
+// NOTE: This may be called more than once, and so should be non-destructive
+async function updateGlobalObject(opts) {
+  global.artillery = global.artillery || {};
 
-// main();
+  global.artillery.runtimeOptions = global.artillery.runtimeOptions || {};
+  global.artillery.runtimeOptions.extendedHTTPMetrics =
+    typeof process.env.ARTILLERY_EXTENDED_HTTP_METRICS !== 'undefined';
+
+  global.artillery.metrics = global.artillery.metrics || {};
+  global.artillery.metrics.event = async function (msg, opts) {
+    if (opts.level === 'error') {
+      console.log(chalk.red(msg));
+    } else {
+      console.log(msg);
+    }
+  };
+
+  global.artillery.util = global.artillery.util || {};
+
+  global.artillery.util.template = require('./lib/engine_util').template;
+
+  global.artillery.plugins = global.artillery.plugins || [];
+
+  global.artillery.extensionEvents = global.artillery.extensionEvents || [];
+
+  global.artillery.ext =
+    global.artillery.ext ||
+    async function (event) {
+      // TODO: Validate events object
+      this.extensionEvents.push(event);
+    };
+
+  global.artillery.globalEvents =
+    global.artillery.globalEvents || new EventEmitter();
+  global.artillery.__SSMS = require('./lib/ssms').SSMS;
+
+  if (typeof global.artillery.suggestedExitCode === 'undefined') {
+    Object.defineProperty(global.artillery, 'suggestedExitCode', {
+      get() {
+        return global.artillery._exitCode;
+      },
+      set(code) {
+        global.artillery._exitCode = code;
+        if (typeof global.artillery._workerThreadSend === 'function') {
+          global.artillery._workerThreadSend({ event: 'setSuggestedExitCode', code: code });
+        }
+      }
+    });
+  }
+}
+
+async function main() {
+  await updateGlobalObject();
+}
+
+main();
 
 module.exports = {
   runner: require('./lib/runner'),
   engine_util: require('./lib/engine_util'),
   engine_http: require('./lib/engine_http'),
   ssms: require('./lib/ssms'),
-  isIdlePhase: require('./lib/is-idle-phase')
+  isIdlePhase: require('./lib/is-idle-phase'),
+  updateGlobalObject
 };
