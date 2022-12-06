4fffe2ad0a45e9f85d527b6294f3e6969acbb28c
juanig1 <jgil@artillery.io>
2022-12-06 15:45:08 -0300

    test: re-add tests now running in pure js

--- packages/artillery/test/cli/_helpers.js
+++ packages/artillery/test/cli/_helpers.js
@@ -1,15 +1,28 @@
 const sh = require('execa');
 const temp = require('temp').track();
 const fs = require('fs');
+const path = require('path');
 const { getBinPathSync } = require('get-bin-path');
 const a9path = getBinPathSync();
 
-async function a9(args) {
-  const fn = temp.path({ suffix: '.txt' });
-  const c = sh(a9path, args);
-  c.stdout.pipe(fs.createWriteStream(fn));
-  const result = await c;
-  return result;
+async function execute(args, options) {
+  try {
+    const fn = temp.path({ suffix: '.txt' });
+    const c = sh(a9path, args, options);
+    c.stdout.pipe(fs.createWriteStream(fn));
+    const result = await c;
+    return [0, result];
+  } catch (err) {
+    return [err.code, err.stderr];
+  }
+}
+async function deleteFile(path) {
+  fs.unlinkSync(path);
+  return true;
 }
 
-module.exports = { a9 };
+async function getRootPath(filename) {
+  return path.resolve(__dirname, '..', '..', filename);
+}
+
+module.exports = { execute, deleteFile, getRootPath };
