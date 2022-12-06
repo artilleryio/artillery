f3f9aeed09aa72a99e75e25961dfc24a558d48bd
juanig1 <jgil@artillery.io>
2022-12-06 15:43:41 -0300

    refactor: shorten scripts if possible; update target location

---
+++ packages/artillery/test/targets/gh_215_target.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const main = async () => {
+  const server = Hapi.server({ port: 3004, host: '127.0.0.1' });
+  server.route({
+    path: '/api/v1/register',
+    method: 'POST',
+    handler: register
+  });
+  await server.start();
+  return server;
+};
+
+main()
+  .then((server) => console.log(`Listening on ${server.info.uri}`))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+function register(req, h) {
+  return { status: 'success' };
+}
