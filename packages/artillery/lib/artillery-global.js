6285199bac49bf0eb9bcdabfe094423180b50227
hveldstra <h@veldstra.org>
2022-12-11 20:51:39 +0000

    feat: re-add globals used by other internal packages

--- packages/artillery/lib/artillery-global.js
+++ packages/artillery/lib/artillery-global.js
@@ -3,6 +3,8 @@ const telemetry = require('./telemetry').init();
 
 const { updateGlobalObject } = require('@artilleryio/int-core');
 
+const { parseScript, readScript } = require('./util');
+
 async function createGlobalObject(opts) {
   await updateGlobalObject({
     version,
@@ -13,6 +15,12 @@ async function createGlobalObject(opts) {
   global.artillery.runtimeOptions.legacyReporting =
     typeof process.env.ARTILLERY_USE_LEGACY_REPORT_FORMAT !== 'undefined';
   global.artillery._workerThreadSend = global.artillery._workerThreadSend || null;
+
+  // TODO: Refactor these special fields away
+  global.artillery.__util = global.artillery.__util || {};
+  global.artillery.__util.parseScript = parseScript;
+  global.artillery.__util.readScript = readScript;
+  global.artillery.__createReporter = require('./console-reporter');
 
   global.artillery._exitCode = 0;
 
