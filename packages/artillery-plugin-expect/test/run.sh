ffbf0372176241fe1f7213ce0a16eb19ad7dff7f
hveldstra <h@veldstra.org>
2023-02-03 14:39:09 +0000

    ci: require Docker. it's 2023

--- packages/artillery-plugin-expect/test/run.sh
+++ packages/artillery-plugin-expect/test/run.sh
@@ -9,24 +9,14 @@ MOCKINGJAY_VERSION="1.12.0"
 # Start mock server
 #
 
-mock_server_pid=
-mock_server_status=
-
 cleanup() {
-    kill $mock_server_pid || true
+
     docker stop mockingjay || true
 }
 
 trap cleanup EXIT
 
-if [[ ! -z ${CIRCLECI:-""} ]] ; then
-    curl -L -o mockingjay-server "https://github.com/quii/mockingjay-server/releases/download/$MOCKINGJAY_VERSION/linux_386_mockingjay-server"
-    chmod +x mockingjay-server
-    ./mockingjay-server --config ./test/mock-pets-server.yaml &
-    mock_server_pid=$!
-else
-    docker run --name mockingjay -p 9090:9090 -v "$DIR":/data "quii/mockingjay-server:$MOCKINGJAY_VERSION" --config /data/mock-pets-server.yaml &
-fi
+docker run --name mockingjay -p 9090:9090 -v "$DIR":/data "quii/mockingjay-server:$MOCKINGJAY_VERSION" --config /data/mock-pets-server.yaml &
 
 sleep 10
 
