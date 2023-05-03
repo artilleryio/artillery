8b1acb0718117ca7745fb42d25bd04f744c77d6b
hveldstra <h@veldstra.org>
2023-05-03 16:14:18 +0100

    feat: enable cloud reporting for Fargate
    This enables Artillery Cloud reporting via the integration in this repo rather than the custom Artillery Pro one.

--- packages/artillery/lib/cmds/aws/run.js
+++ packages/artillery/lib/cmds/aws/run.js
@@ -2,15 +2,19 @@ const { Command, flags } = require('@oclif/command');
 
 const telemetry = require('../../telemetry').init();
 
+const { Plugin: CloudPlugin } = require('../../platform/cloud/cloud');
+
 var tryRequire = require('try-require');
 const Pro = tryRequire('artillery-pro');
 
 class RunCommand extends Command {
-  static aliases = ['run-test', 'run-cluster'];
+  static aliases = ['run-test', 'run-cluster', 'run:fargate'];
   // Enable multiple args:
   static strict = false;
 
   async run() {
+    new CloudPlugin();
+
     if (!Pro) {
       console.error(
         'Error: This command requires artillery-pro to be installed\n'
