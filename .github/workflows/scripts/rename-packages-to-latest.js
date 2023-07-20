05376e4dc6587e1d9550b9c98d0d64a4acaf5eb8
Bernardo Guerreiro <bernardoguerr@gmail.com>
2023-07-20 11:28:01 +0100

    ci(publish): expand rename script to work on all packages

--- .github/workflows/scripts/rename-packages-to-latest.js
+++ .github/workflows/scripts/rename-packages-to-latest.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
-const packageJsonPath = '../../../packages/artillery/package.json';
-const package = require(packageJsonPath);
 const path = require('path');
+
+const packageJsonRelativePath = `../../../packages/${process.env.PACKAGE_FOLDER_NAME}/package.json`;
+const packageJsonFullPath = path.join(__dirname, packageJsonRelativePath);
+let package;
+
+if (!fs.existsSync(packageJsonFullPath)) {
+  throw new Error(
+    `Path ${packageJsonRelativePath} does not exist! Please ensure you pass the correct PACKAGE_FOLDER_NAME environment variable. Perhaps your folder name has changed?`
+  );
+} else {
+  package = require(packageJsonRelativePath);
+}
 
 Object.keys(package.dependencies).forEach((key) => {
   if (package.dependencies[key] == '*') {
@@ -9,7 +19,4 @@ Object.keys(package.dependencies).forEach((key) => {
   }
 });
 
-fs.writeFileSync(
-  `${path.join(__dirname, packageJsonPath)}`,
-  JSON.stringify(package, null, 2)
-);
+fs.writeFileSync(`${packageJsonFullPath}`, JSON.stringify(package, null, 2));
