89828037924cf727dd78f5907db09b4286da6ae5
Artem Zakharchenko <kettanaito@gmail.com>
2023-08-25 13:47:45 +0200

    fix(types): rename "followRedirects" to "followRedirect"

--- packages/types/definitions.ts
+++ packages/types/definitions.ts
@@ -491,7 +491,7 @@ export type DefaultHttpRequest = {
    * Set this option to `false` to stop following redirects.
    * @title Disable redirect following
    */
-  followRedirects?: false;
+  followRedirect?: false;
   /**
    * @title Capture
    */
