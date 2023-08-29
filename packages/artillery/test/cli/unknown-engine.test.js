efd695447118dfef8971262a90e835a9acb4e62a
Artem Zakharchenko <kettanaito@gmail.com>
2023-08-29 15:14:30 +0200

    test: assert output.stdout instead of output

--- packages/artillery/test/cli/unknown-engine.test.js
+++ packages/artillery/test/cli/unknown-engine.test.js
@@ -4,17 +4,17 @@ const { execute } = require('../cli/_helpers.js');
 tap.test(
   'Throws when encountered an unknown engine in scenarios',
   async (t) => {
-    const [exitCode, error] = await execute([
+    const [exitCode, output] = await execute([
       'run',
       'test/scripts/unknown_engine.json'
     ]);
 
-    console.log({ exitCode, error }, error.message);
-
-    t.ok(exitCode !== 0);
-    t.same(
-      error.message,
-      `Failed to run scenario "0": unknown engine "playwright". Did you forget to include it in "config.engines.playwright"?`
+    t.ok(exitCode === 11);
+    t.ok(
+      output.stdout.includes(
+        'Failed to run scenario "0": unknown engine "playwright". Did you forget to include it in "config.engines.playwright"?'
+      ),
+      'Error message about missing "config[engineName]" is not being printed'
     );
   }
 );
