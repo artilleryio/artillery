6e3239235ea1d70e310163ab40d0d29a7bf32b4b
Bernardo Guerreiro <bernardoguerr@gmail.com>
2023-09-08 15:18:17 +0200

    feat(schema): add titles and description to playwright engine

--- packages/types/schema/engines/playwright.js
+++ packages/types/schema/engines/playwright.js
@@ -5,16 +5,40 @@ const Joi = require('joi').defaults((schema) =>
 const artilleryStringNumber = Joi.alternatives(Joi.number(), Joi.string());
 
 const PlaywrightSchemaObject = {
-  testFunction: Joi.string(),
+  testFunction: Joi.string()
+    .meta({ title: 'Test function' })
+    .description('Equivalent to flowFunction.'),
   flowFunction: Joi.string()
+    .meta({ title: 'Flow function' })
+    .description('Equivalent to testFunction.')
 };
 
 const PlaywrightConfigSchema = Joi.object({
-  aggregateByName: Joi.alternatives(Joi.boolean(), Joi.string()),
-  defaultTimeout: artilleryStringNumber,
-  defaultNavigationTimeout: artilleryStringNumber,
-  launchOptions: Joi.object(),
+  aggregateByName: Joi.alternatives(Joi.boolean(), Joi.string())
+    .meta({ title: 'Aggregate by name' })
+    .description(
+      'Aggregate Artillery metrics by test scenario name.\nhttps://www.artillery.io/docs/reference/engines/playwright#aggregate-metrics-by-scenario-name'
+    ),
+  defaultTimeout: artilleryStringNumber
+    .meta({ title: 'Default timeout' })
+    .description(
+      'Default maximum time (in seconds) for all Playwright methods accepting the `timeout` option.\nhttps://playwright.dev/docs/api/class-browsercontext#browser-context-set-default-timeout'
+    ),
+  defaultNavigationTimeout: artilleryStringNumber
+    .meta({ title: 'Default navigation timeout' })
+    .description(
+      'Default maximum navigation time (in seconds) for Playwright navigation methods, like `page.goto()`.\nhttps://playwright.dev/docs/api/class-browsercontext#browser-context-set-default-navigation-timeout'
+    ),
+  launchOptions: Joi.object()
+    .meta({ title: 'Playwright launch options' })
+    .description(
+      'Arguments for the `browser.launch()` call in Playwright.\nhttps://playwright.dev/docs/api/class-browsertype#browser-type-launch'
+    ),
   contextOptions: Joi.object()
+    .meta({ title: 'Playwright context options' })
+    .description(
+      'Arguments for the `browser.newContext()` call in Playwright.\nhttps://playwright.dev/docs/api/class-browser#browser-new-context'
+    )
 });
 
 module.exports = {
