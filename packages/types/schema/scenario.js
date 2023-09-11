304987c5f338b23f979f40a251ca8e6aa69700f9
Bernardo Guerreiro <bernardoguerr@gmail.com>
2023-09-11 11:20:10 +0200

    refactor(schema): various scenario changes

--- packages/types/schema/scenario.js
+++ packages/types/schema/scenario.js
@@ -8,54 +8,63 @@ const { SocketIoFlowItemSchema } = require('./engines/socketio');
 const { PlaywrightSchemaObject } = require('./engines/playwright');
 
 const ScenarioSchema = Joi.object({
-  name: Joi.string(),
+  name: Joi.string().meta({ title: 'Scenario Name' }),
   // engine: Joi.alternatives().conditional('engine', { is: Joi.alternatives('socketio', 'ws', 'http'), then: Joi.alternatives('socketio', 'ws', 'http'), otherwise: Joi.string().invalid('socketio', 'ws', 'http')}),//TODO:maybe improve this?
   // flow: Joi.array().items(Joi.object()),
-  engine: Joi.alternatives(
-    'http',
-    'ws',
-    'websocket',
-    'socketio',
-    'playwright',
-    Joi.string()
-  ),
-  beforeScenario: Joi.array().items(Joi.string()).single(), //TODO:review this
-  afterScenario: Joi.array().items(Joi.string()).single() //TODO:review this
+  //   engine: Joi.alternatives(
+  //     'http',
+  //     'ws',
+  //     'websocket',
+  //     'socketio',
+  //     'playwright',
+  //     null,
+  //     Joi.string()
+  //   ),
+  weight: Joi.alternatives(Joi.string(), Joi.number()),
+  beforeScenario: Joi.alternatives(
+    Joi.string(),
+    Joi.array().items(Joi.string()).single()
+  )
+    .meta({ title: 'beforeScenario hook' })
+    .description(
+      'Custom Javascript functions to run before each scenario\nhttps://www.artillery.io/docs/reference/engines/http#function-actions-and-beforescenario--afterscenario-hooks'
+    ), //TODO:review this
+  afterScenario: Joi.alternatives(
+    Joi.string(),
+    Joi.array().items(Joi.string()).single()
+  )
+    .meta({ title: 'afterScenario hook' })
+    .description(
+      'Custom Javascript functions to run after each scenario\nhttps://www.artillery.io/docs/reference/engines/http#function-actions-and-beforescenario--afterscenario-hooks'
+    ) //TODO:review this
 })
-  // .when('engine', {
-  //     is: 'http',
-  //     then: Joi.object({
-  //         engine: Joi.string().valid('http'),
-  //         flow: Joi.array().items(HttpFlowItemSchema).required().meta({title: 'HTTP Engine Flow'})
-  //     })
-  // })
-  // .when('engine', {
-  //     is: Joi.any().valid(null, ""),
-  //     then: Joi.object({
-  //         engine: Joi.any().valid(null, ""),
-  //         flow: Joi.array().items(HttpFlowItemSchema).required().meta({title: 'HTTP Engine Flow (Default)'})
-  //     })
-  // })
+  .when(Joi.object({ engine: Joi.string().valid(null, '') }), {
+    then: Joi.object({
+      //   engine: Joi.string().valid(null, ''),
+      flow: Joi.array()
+        .items(HttpFlowItemSchema)
+        .required()
+        .meta({ title: 'HTTP Engine Flow (Default)' })
+    }) //TODO: figure out why it's not defaulting to default engine
+    // then: Joi.object({
+    //     engine: Joi.any().valid(null, ""),
+    //     flow: Joi.array().items(Joi.alternatives(HttpFlowItemSchema, HttpLoopSchema)).required().meta({title: "HTTP Flow"})
+    // }),
+  })
   .when(Joi.object({ engine: Joi.string().valid('http') }), {
     then: Joi.object({
-      engine: Joi.string().valid('http'),
+      engine: Joi.string().valid('http').meta({ title: 'HTTP Engine' }),
       flow: Joi.array()
         .items(HttpFlowItemSchema)
         .required()
         .meta({ title: 'HTTP Engine Flow' })
     })
-    // then: Joi.object({
-    //     engine: Joi.alternatives('http'),
-    //     flow: Joi.array().items(Joi.alternatives(HttpFlowItemSchema, HttpLoopSchema)).required().meta({title: "HTTP Flow"})
-    // }),
-    // then: Joi.object({
-
-    // })
-    // then: Joi.when(Joi.object({}))
   })
   .when(Joi.object({ engine: Joi.string().valid('ws', 'websocket') }), {
     then: Joi.object({
-      engine: Joi.string().valid('ws', 'websocket'),
+      engine: Joi.string()
+        .valid('ws', 'websocket')
+        .meta({ title: 'Websocket Engine' }),
       flow: Joi.array()
         .items(WsFlowItemSchema)
         .required()
@@ -65,7 +74,7 @@ const ScenarioSchema = Joi.object({
   })
   .when(Joi.object({ engine: Joi.string().valid('socketio') }), {
     then: Joi.object({
-      engine: Joi.string().valid('socketio'),
+      engine: Joi.string().valid('socketio').meta({ title: 'SocketIo Engine' }),
       flow: Joi.array()
         .items(SocketIoFlowItemSchema)
         .required()
@@ -80,19 +89,6 @@ const ScenarioSchema = Joi.object({
       ...PlaywrightSchemaObject
     })
   })
-  .when(Joi.object({ engine: Joi.any().valid(null, '') }), {
-    then: Joi.object({
-      engine: Joi.any().valid(null, ''),
-      flow: Joi.array()
-        .items(HttpFlowItemSchema)
-        .required()
-        .meta({ title: 'HTTP Engine Flow (Default)' })
-    })
-    // then: Joi.object({
-    //     engine: Joi.any().valid(null, ""),
-    //     flow: Joi.array().items(Joi.alternatives(HttpFlowItemSchema, HttpLoopSchema)).required().meta({title: "HTTP Flow"})
-    // }),
-  })
   .when(
     Joi.object({
       engine: Joi.string().not(
@@ -105,20 +101,23 @@ const ScenarioSchema = Joi.object({
     }),
     {
       then: Joi.object({
-        engine: Joi.string(),
+        engine: Joi.string().meta({ title: 'Custom Engine' }),
         // flow: Joi.array().items(Joi.any()).required().meta({title: 'Generic Engine Flow'})
-        flow: Joi.any().meta({ title: 'Generic Engine Flow' }) //TODO: decide if flow should be required here? probably not
+        flow: Joi.any().meta({ title: 'Custom Engine Flow' }) //TODO: decide if flow should be required here? probably not
       })
     }
   );
 
 // TODO: PR in joi-to-json repo for converting deprecated and default
 
-//TODO: loops are still not working well. I am not sure why it doesnt detect things correctly
 //TODO: Lets do all the descriptions
-//TODO: check why it doesnt accuse  when you do get: url: number
-//TODO: type socketio and ws
+
+//TODO: type this with engine flows
+const BeforeAfterScenarioSchema = Joi.object({
+  flow: Joi.array().items(Joi.any())
+});
 
 module.exports = {
-  ScenarioSchema
+  ScenarioSchema,
+  BeforeAfterScenarioSchema
 };
