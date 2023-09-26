a5a0c9dc692fd01115688a8cb99d0bef28024288
Bernardo Guerreiro <39738771+bernardobridge@users.noreply.github.com>
2023-09-26 20:00:33 +0100

    feat(schema): include schema dialect (#2169)

--- packages/types/generate-schema.js
+++ packages/types/generate-schema.js
@@ -3,7 +3,7 @@ const path = require('path');
 const parse = require('joi-to-json');
 const { schema } = require('./schema/index');
 
-const jsonSchema = parse(schema);
+const jsonSchema = parse(schema, 'json', {}, { includeSchemaDialect: true });
 
 fs.writeFileSync(
   path.join(__dirname, './schema.json'),
