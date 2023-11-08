717d290e167f61b335a189623560e949e2c3e774
Bernardo Guerreiro <bernardoguerr@gmail.com>
2023-11-08 17:34:37 +0000

    style: fix linting issue on index file

--- packages/artillery-plugin-ensure/index.js
+++ packages/artillery-plugin-ensure/index.js
@@ -65,19 +65,21 @@ class EnsurePlugin {
         global.artillery.globalEvents.emit('checks', checkTests);
 
         checkTests
-          .sort((a, b) => (a.result < b.result) ? 1 : -1)
+          .sort((a, b) => (a.result < b.result ? 1 : -1))
           .forEach((check) => {
-          if (check.result !== 1) {
-            global.artillery.log(
-              `${chalk.red('fail')}: ${check.original}${check.strict ? '' : ' (optional)'}`
-            );
-            if (check.strict) {
-              global.artillery.suggestedExitCode = 1;
+            if (check.result !== 1) {
+              global.artillery.log(
+                `${chalk.red('fail')}: ${check.original}${
+                  check.strict ? '' : ' (optional)'
+                }`
+              );
+              if (check.strict) {
+                global.artillery.suggestedExitCode = 1;
+              }
+            } else {
+              global.artillery.log(`${chalk.green('ok')}: ${check.original}`);
             }
-          } else {
-            global.artillery.log(`${chalk.green('ok')}: ${check.original}`);
-          }
-        });
+          });
       }
     });
   }
@@ -139,7 +141,7 @@ class EnsurePlugin {
       .filter((k) => LEGACY_CONDITIONS.indexOf(k) > -1)
       .forEach((k) => {
         const metricName = `http.response_time.${k}`;
-        const maxValue = parseInt(checks[k]);
+        const maxValue = parseInt(checks[k], 10);
         let f = () => {};
         try {
           f = filtrex(`${metricName} < ${maxValue}`);
