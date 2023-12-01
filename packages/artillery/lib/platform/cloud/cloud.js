ffdd0a1cc6b3bb346c66dcbd97f2a5cab6daf93d
Bernardo Guerreiro <39738771+bernardobridge@users.noreply.github.com>
2023-12-01 18:54:16 +0000

    refactor(cloud): add final log to cli when test run cancelled (#2324)
    * refactor(cloud): show test run stop log at the end of the run
    
    * chore: update package-lock
    
    * refactor(cloud): break final log message into multiple lines
    
    * refactor: cleanup code for logging

--- packages/artillery/lib/platform/cloud/cloud.js
+++ packages/artillery/lib/platform/cloud/cloud.js
@@ -33,7 +33,7 @@ class ArtilleryCloudPlugin {
       'x-auth-token': this.apiKey
     };
     this.unprocessedLogsCounter = 0;
-    this.hasCancellationRequest = false;
+    this.cancellationRequestedBy = '';
 
     let testEndInfo = {};
     global.artillery.globalEvents.on('test:init', async (testInfo) => {
@@ -160,7 +160,11 @@ class ArtilleryCloudPlugin {
           status: opts.earlyStop ? 'EARLY_STOP' : 'COMPLETED'
         });
 
-        console.log(`\nRun URL: ${testEndInfo.testRunUrl}`);
+        console.log('\n');
+        if (this.cancellationRequestedBy) {
+          console.log(`Test run stopped by ${this.cancellationRequestedBy}.`);
+        }
+        console.log(`Run URL: ${testEndInfo.testRunUrl}`);
       }
     });
 
@@ -179,7 +183,7 @@ class ArtilleryCloudPlugin {
 
   setGetStatusInterval() {
     const interval = setInterval(async () => {
-      if (this.hasCancellationRequest) {
+      if (this.cancellationRequestedBy) {
         return;
       }
       const res = await this._getLoadTestStatus();
@@ -196,7 +200,7 @@ class ArtilleryCloudPlugin {
       console.log(
         `WARNING: Artillery Cloud user ${res.cancelledBy} requested to stop the test. Stopping test run - this may take a few seconds.`
       );
-      this.hasCancellationRequest = true;
+      this.cancellationRequestedBy = res.cancelledBy;
       global.artillery.suggestedExitCode = 8;
       await global.artillery.shutdown({ earlyStop: true });
     }, 5000);
