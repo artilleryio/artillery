f8202140b55693b39bcd3f5840e104b08432a6a4
Bernardo Guerreiro <bernardoguerr@gmail.com>
2023-12-08 15:54:28 +0000

    feat(fargate): install ddagent in workers if DD_API_KEY is present

---
+++ packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+install_and_configure_dd_agent() {
+    # Check if DD_API_KEY is set in the environment
+    if [ -z "${DD_API_KEY}" ]; then
+        echo "DD_API_KEY is not set. Aborting."
+        return 0
+    fi
+
+    # Download and install the Datadog Agent
+    DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$DD_API_KEY bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_mac_os.sh)"
+
+    # Specify the YAML file to modify
+    yaml_file="$HOME/.datadog-agent/datadog.yaml"
+
+    # Configuration to add
+    config_to_add='otlp_config:\n  receiver:\n    protocols:\n      http:\n        endpoint: localhost:4318\n'
+
+    # Append the configuration to the file
+    printf "%s\n" "$config_to_add" >> "$yaml_file"
+
+    echo "Configuration added to $yaml_file."
+}
+
+# You can now call this function in your script
+# install_and_configure_dd_agent
