72ee63e67072a564babe5c347e110584d39da753
Nidhi Work <fazlic.ines@gmail.com>
2023-12-15 14:38:29 +0100

    refactor: try beta version env var

--- packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
+++ packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
@@ -8,29 +8,35 @@ start_and_configure_dd_agent() {
 
     # Specify the YAML file to modify
     yaml_file="/etc/datadog-agent/datadog.yaml"
-    hostname="task-$1"
-
+    # hostname="task-$1"
+    export DD_OTLP_CONFIG_TRACES_ENABLED="true"
     # Reference of configuration to add
     # https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml
 
     # Update API Key in the config file
-    yq -i ".api_key = \"$DD_API_KEY\"" "$yaml_file"
+    # yq -i ".api_key = \"$DD_API_KEY\"" "$yaml_file"
 
     # Add/Update hostname
-    yq -i ".hostname = \"$hostname\"" "$yaml_file"
+    # yq -i ".hostname = \"$hostname\"" "$yaml_file"
+    export DD_HOSTNAME="task-$1"
 
     # Add otlp config
-    yq -i ".otlp_config.receiver.protocols.http.endpoint = \"localhost:4318\"" "$yaml_file"
-    yq -i ".otlp_config.receiver.protocols.grpc.endpoint = \"localhost:4317\"" "$yaml_file"
+    # yq -i ".otlp_config.receiver.protocols.http.endpoint = \"localhost:4318\"" "$yaml_file"
+    # yq -i ".otlp_config.receiver.protocols.grpc.endpoint = \"localhost:4317\"" "$yaml_file"
 
+    export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT="localhost:4318"
+    export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT="localhost:4317"
     # Add apm_config trace_buffer
-    yq -i ".apm_config.trace_buffer = 100" "$yaml_file"
+    # yq -i ".apm_config.trace_buffer = 100" "$yaml_file"
+    export DD_APM_TRACE_BUFFER="100"
 
     # TODO investigate if max_traces_per_second needs adjusting 
     # TODO review other config options. Reference https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml#L1279C1-L1287C30
 
     echo "Starting datadog-agent..."
     service datadog-agent start
+
+    echo $yaml_file
 
     echo "Started datadog-agent successfully!"
 }
