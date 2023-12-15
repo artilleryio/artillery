972cf8a6de2b041553b75ef450d69344fe8a9d1f
Nidhi Work <fazlic.ines@gmail.com>
2023-12-15 10:23:51 +0100

    refactor: configure dd with env variables

--- packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
+++ packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
@@ -1,81 +1,38 @@
-#!/usr/bin/env bash
-
-# install_and_configure_dd_agent() {
-
-#     #TODO probably rename this
-#     export DD_HOSTNAME=task-$1
-#     # export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT=DD_HOSTNAME:4318
-#     echo "DD_HOSTNAME set to $DD_HOSTNAME."
-#     #TODO get rid of container name
-#     echo "CONTAINER NAME IS $2"
-
-#     # Download and install the Datadog Agent
-#     # Script 7 downloads the agent version 7 (currently latest)
-#     ## TODO Maybe DD_SITE also needs to be overriden
-#     DD_INSTALL_ONLY=true DD_SITE="datadoghq.com" DD_API_KEY=$DD_API_KEY bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
-# }
 
 start_and_configure_dd_agent() {
     # Check if DD_API_KEY is set in the environment
-    if [ -z "${DD_API_KEY}" ]; then
+    if [ -z "${DD_API_KEY-}" ]; then
+        echo "DD_API_KEY not set. Not running Datadog Agent."
         return 0
     fi
-        # Specify the YAML file to modify
-    yaml_file="/etc/datadog-agent/datadog.yaml"
-    hostname="task-$1"
+    # Specify the YAML file to modify
+    # yaml_file="/etc/datadog-agent/datadog.yaml"
+    # hostname="task-$1"
 
-    # Configuration to add
+    # Reference of configuration to add
     # https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml
-    # and ...
-#     config_to_add=$(cat <<EOF
-
-# api_key: $DD_API_KEY
-# hostname: $hostname
-# otlp_config:
-#   receiver:
-#     protocols:
-#       http:
-#         endpoint: $2:4318
-# apm_config:
-#   trace_buffer: 100
-# EOF
-#     )
-
-    # Append the configuration to the file
-    # echo "$config_to_add" >> "$yaml_file"
 
     # Update API Key in the config file
-    yq -i ".api_key = \"$DD_API_KEY\"" "$yaml_file"
+    # yq -i ".api_key = \"$DD_API_KEY\"" "$yaml_file"
 
     # Add/Update hostname
-    yq -i ".hostname = \"$hostname\"" "$yaml_file"
+    # yq -i ".hostname = \"$hostname\"" "$yaml_file"
+    export DD_HOSTNAME="task-$1"
 
     # Add otlp config
-    yq -i ".otlp_config.receiver.protocols.http.endpoint = \"$2:4318\"" "$yaml_file"
+    # yq -i ".otlp_config.receiver.protocols.http.endpoint = \"localhost:4318\"" "$yaml_file"
+    export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT="0.0.0.0:4318"
+    export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT="0.0.0.0:4317"
 
     # Add apm_config trace_buffer
-    yq -i ".apm_config.trace_buffer = 100" "$yaml_file"
+    # yq -i ".apm_config.trace_buffer = 100" "$yaml_file"
+    export DD_APM_TRACE_BUFFER=100
 
-    echo "Configuration added to $yaml_file."
+    # TODO investigate if max_traces_per_second needs adjusting 
+    # TODO review other config options. Reference https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml#L1279C1-L1287C30
+
     echo "Starting datadog-agent..."
-    #TODO check how verbose starting is in terms of logs
-    cat $yaml_file
     service datadog-agent start
 
-    echo "Started datadog-agent."
+    echo "Started datadog-agent successfully!"
 }
-
-# https://github.com/DataDog/datadog-agent/issues/3940
-## TODO: Probably remove this
-# wait_for_dd_agent_to_flush() {
-#     if [ -z "${DD_API_KEY}" ]; then
-#         return 0
-#     fi
-#     # Wait for the agent to flush its data
-#     echo "Waiting for the agent to flush its data..."
-#     sleep 35
-#     echo "Done waiting."
-# }
-
-# You can now call this function in your script
-# install_and_configure_dd_agent
