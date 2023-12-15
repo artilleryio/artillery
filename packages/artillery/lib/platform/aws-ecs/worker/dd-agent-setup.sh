83eaca65c2a2c0e7c66ac02df0ff0f4120a5e6d8
Nidhi Work <fazlic.ines@gmail.com>
2023-12-15 15:43:21 +0100

    refactor: try all apm instrumentation enabled

--- packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
+++ packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
@@ -33,7 +33,7 @@ start_and_configure_dd_agent() {
     # TODO investigate if max_traces_per_second needs adjusting 
     # TODO review other config options. Reference https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml#L1279C1-L1287C30
 
-    DD_SITE="datadoghq.com" DD_APM_ENABLED="true" DD_OTLP_CONFIG_TRACES_ENABLED="true" DD_HOSTNAME="task-$1" DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT="localhost:4318" DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT="localhost:4317" DD_APM_TRACE_BUFFER="100" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+    DD_APM_INSTRUMENTATION_ENABLED="all" DD_SITE="datadoghq.com" DD_APM_ENABLED="true" DD_OTLP_CONFIG_TRACES_ENABLED="true" DD_HOSTNAME="task-$1" DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT="localhost:4318" DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT="localhost:4317" DD_APM_TRACE_BUFFER="100" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
 
     echo "Starting datadog-agent..."
     service datadog-agent start
