96a590696cb1b8b5d16dfa0aaf930db78b61af02
Nidhi Work <fazlic.ines@gmail.com>
2023-12-15 14:52:44 +0100

    refactor: try everything inline

--- packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
+++ packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
@@ -9,7 +9,7 @@ start_and_configure_dd_agent() {
     # Specify the YAML file to modify
     yaml_file="/etc/datadog-agent/datadog.yaml"
     # hostname="task-$1"
-    export DD_OTLP_CONFIG_TRACES_ENABLED="true"
+    # export DD_OTLP_CONFIG_TRACES_ENABLED="true"
     # Reference of configuration to add
     # https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml
 
@@ -18,23 +18,23 @@ start_and_configure_dd_agent() {
 
     # Add/Update hostname
     # yq -i ".hostname = \"$hostname\"" "$yaml_file"
-    export DD_HOSTNAME="task-$1"
+    # export DD_HOSTNAME="task-$1"
 
     # Add otlp config
     # yq -i ".otlp_config.receiver.protocols.http.endpoint = \"localhost:4318\"" "$yaml_file"
     # yq -i ".otlp_config.receiver.protocols.grpc.endpoint = \"localhost:4317\"" "$yaml_file"
 
-    export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT="localhost:4318"
-    export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT="localhost:4317"
+    # export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT="localhost:4318"
+    # export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT="localhost:4317"
     # Add apm_config trace_buffer
     # yq -i ".apm_config.trace_buffer = 100" "$yaml_file"
-    export DD_APM_TRACE_BUFFER="100"
+    # export DD_APM_TRACE_BUFFER="100"
 
     # TODO investigate if max_traces_per_second needs adjusting 
     # TODO review other config options. Reference https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml#L1279C1-L1287C30
 
     echo "Starting datadog-agent..."
-    service datadog-agent start
+    DD_APM_INSTRUMENTATION_ENABLED="host" DD_OTLP_CONFIG_TRACES_ENABLED="true" DD_HOSTNAME="task-$1" DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT="localhost:4318" DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT="localhost:4317" DD_APM_TRACE_BUFFER="100" service datadog-agent start
 
     echo $yaml_file
 
