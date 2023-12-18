e66a72b431bc8b4dde3dbb150d2b5d0666171477
hveldstra <h@veldstra.org>
2023-12-18 15:45:50 +0000

    feat: configure agent with env vars

--- packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
+++ packages/artillery/lib/platform/aws-ecs/worker/dd-agent-setup.sh
@@ -6,6 +6,18 @@ start_and_configure_dd_agent() {
         return 0
     fi
 
+    service --status-all
+
+    export DD_API_KEY="$DD_API_KEY"
+    export DD_HOSTNAME="task-$1"
+    export DD_OTLP_CONFIG_TRACES_ENABLED=true
+    export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT=0.0.0.0:4318
+    export DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GCPR_ENDPOINT=0.0.0.0:4317
+    export DD_APM_ENABLED=true
+    export DD_APM_RECEIVER_PORT=8126
+    export DD_APM_TRACE_BUFFER=100
+    export DD_SITE=datadoghq.com
+
     # Specify the YAML file to modify
     yaml_file="/etc/datadog-agent/datadog.yaml"
     hostname="task-$1"
@@ -14,17 +26,17 @@ start_and_configure_dd_agent() {
     # https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml
 
     # Update API Key in the config file
-    yq -i ".api_key = \"$DD_API_KEY\"" "$yaml_file"
+    # yq -i ".api_key = \"$DD_API_KEY\"" "$yaml_file"
 
-    # Add/Update hostname
-    yq -i ".hostname = \"$hostname\"" "$yaml_file"
+    # # Add/Update hostname
+    # yq -i ".hostname = \"$hostname\"" "$yaml_file"
 
-    # Add otlp config
-    yq -i ".otlp_config.receiver.protocols.http.endpoint = \"localhost:4318\"" "$yaml_file"
-    yq -i ".otlp_config.receiver.protocols.grpc.endpoint = \"localhost:4317\"" "$yaml_file"
+    # # Add otlp config
+    # yq -i ".otlp_config.receiver.protocols.http.endpoint = \"localhost:4318\"" "$yaml_file"
+    # yq -i ".otlp_config.receiver.protocols.grpc.endpoint = \"localhost:4317\"" "$yaml_file"
 
-    # Add apm_config trace_buffer
-    yq -i ".apm_config.trace_buffer = 100" "$yaml_file"
+    # # Add apm_config trace_buffer
+    # yq -i ".apm_config.trace_buffer = 100" "$yaml_file"
 
     # TODO investigate if max_traces_per_second needs adjusting 
     # TODO review other config options. Reference https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml#L1279C1-L1287C30
