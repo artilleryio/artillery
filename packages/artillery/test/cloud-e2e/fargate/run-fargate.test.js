dc5ccc679d38da7488c8fbab86f0845bed0515f3
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-01-11 12:01:18 +0000

    test(fargate): add ensure test

--- packages/artillery/test/cloud-e2e/fargate/run-fargate.test.js
+++ packages/artillery/test/cloud-e2e/fargate/run-fargate.test.js
@@ -2,6 +2,7 @@ const { test, before } = require('tap');
 const { $ } = require('zx');
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
 const A9 = process.env.A9 || 'artillery';
 
@@ -39,4 +40,30 @@ test('Run mixed-hierarchy', async (t) => {
     20,
     'Should have 20 "200 OK" responses'
   );
+});
+
+test('Run uses ensure', async (t) => {
+  const jsonReport = path.join(__dirname, `report-${Date.now()}.json`);
+
+  try {
+    await $`${A9} run:fargate ${__dirname}/fixtures/uses-ensure/test.yaml --output ${jsonReport} --count 15`;
+  } catch (output) {
+    t.equal(output.exitCode, 1, 'CLI Exit Code should be 1');
+    t.ok(
+      output.stdout.includes(`${chalk.red('fail')}: http.response_time.p99 < 1`)
+    );
+    t.ok(output.stdout.includes(`${chalk.green('ok')}: p99 < 10000`));
+
+    const report = JSON.parse(fs.readFileSync(jsonReport, 'utf8'));
+    t.equal(
+      report.aggregate.counters['vusers.completed'],
+      300,
+      'Should have 300 total VUs'
+    );
+    t.equal(
+      report.aggregate.counters['http.codes.200'],
+      300,
+      'Should have 300 "200 OK" responses'
+    );
+  }
 });
