8cf57414afd9687de4c729da9bdc22881c0141f6
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-01-11 18:25:52 +0000

    test(lambda): make lambda test report to cloud with tags

--- packages/artillery/test/cloud-e2e/lambda/run-lambda.test.js
+++ packages/artillery/test/cloud-e2e/lambda/run-lambda.test.js
@@ -1,12 +1,14 @@
 const tap = require('tap');
 const { $ } = require('zx');
+const { getTestTags } = require('../../cli/_helpers.js');
 
 tap.test('Run a test on AWS Lambda', async (t) => {
+  const tags = getTestTags(['type:acceptance']);
   const configPath = `${__dirname}/fixtures/quick-loop-with-csv/config.yml`;
   const scenarioPath = `${__dirname}/fixtures/quick-loop-with-csv/blitz.yml`;
 
   const output =
-    await $`artillery run-lambda --count 10 --region eu-west-1 --config ${configPath} ${scenarioPath}`;
+    await $`artillery run-lambda --count 10 --region eu-west-1 --config ${configPath} --record --tags ${tags} ${scenarioPath}`;
 
   t.equal(output.exitCode, 0, 'CLI should exit with code 0');
 
