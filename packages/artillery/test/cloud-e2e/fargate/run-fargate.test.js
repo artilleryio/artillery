901af24b3a11d4de378907598cb1446815d827f5
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-01-16 10:15:20 +0000

    test: try a bom test in windows

--- packages/artillery/test/cloud-e2e/fargate/run-fargate.test.js
+++ packages/artillery/test/cloud-e2e/fargate/run-fargate.test.js
@@ -21,7 +21,7 @@ beforeEach(async (t) => {
 
 test('Run simple-bom', async (t) => {
   const output =
-    await $`${A9} run-fargate ${__dirname}/fixtures/simple-bom/simple-bom.yml --environment test --region eu-west-1 --count 51 --record --tags ${baseTags}`;
+    await $`${A9} run-fargate ${__dirname}/fixtures/simple-bom/simple-bom.yml --environment test --region eu-west-1 --count 9 --record --tags ${baseTags}`;
 
   t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
 
@@ -53,160 +53,160 @@ test('Run mixed-hierarchy', async (t) => {
   );
 });
 
-test('Run uses ensure', async (t) => {
-  try {
-    await $`${A9} run:fargate ${__dirname}/fixtures/uses-ensure/with-ensure.yaml --record --tags ${baseTags} --output ${reportFilePath} --count 15`;
-  } catch (output) {
-    t.equal(output.exitCode, 1, 'CLI Exit Code should be 1');
-    t.ok(
-      output.stdout.includes(`${chalk.red('fail')}: http.response_time.p99 < 1`)
-    );
-    t.ok(output.stdout.includes(`${chalk.green('ok')}: p99 < 10000`));
+// test('Run uses ensure', async (t) => {
+//   try {
+//     await $`${A9} run:fargate ${__dirname}/fixtures/uses-ensure/with-ensure.yaml --record --tags ${baseTags} --output ${reportFilePath} --count 15`;
+//   } catch (output) {
+//     t.equal(output.exitCode, 1, 'CLI Exit Code should be 1');
+//     t.ok(
+//       output.stdout.includes(`${chalk.red('fail')}: http.response_time.p99 < 1`)
+//     );
+//     t.ok(output.stdout.includes(`${chalk.green('ok')}: p99 < 10000`));
 
-    const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
-    t.equal(
-      report.aggregate.counters['vusers.completed'],
-      300,
-      'Should have 300 total VUs'
-    );
-    t.equal(
-      report.aggregate.counters['http.codes.200'],
-      300,
-      'Should have 300 "200 OK" responses'
-    );
-  }
-});
+//     const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
+//     t.equal(
+//       report.aggregate.counters['vusers.completed'],
+//       300,
+//       'Should have 300 total VUs'
+//     );
+//     t.equal(
+//       report.aggregate.counters['http.codes.200'],
+//       300,
+//       'Should have 300 "200 OK" responses'
+//     );
+//   }
+// });
 
-test('Ensure (with new interface) should still run when workers exit from expect plugin (non zero exit code)', async (t) => {
-  //Note: this test uses new ensure plugin interface (config.plugins.ensure) to test that indirectly
+// test('Ensure (with new interface) should still run when workers exit from expect plugin (non zero exit code)', async (t) => {
+//   //Note: this test uses new ensure plugin interface (config.plugins.ensure) to test that indirectly
 
-  try {
-    await $`${A9} run:fargate ${__dirname}/fixtures/cli-exit-conditions/with-expect-ensure.yml --record --tags ${baseTags} --output ${reportFilePath} --count 2`;
-  } catch (output) {
-    t.equal(output.exitCode, 1, 'CLI Exit Code should be 1');
-    t.ok(
-      output.stdout.includes(`${chalk.red('fail')}: http.response_time.p95 < 1`)
-    );
-    t.ok(output.stdout.includes(`${chalk.green('ok')}: p99 < 10000`));
+//   try {
+//     await $`${A9} run:fargate ${__dirname}/fixtures/cli-exit-conditions/with-expect-ensure.yml --record --tags ${baseTags} --output ${reportFilePath} --count 2`;
+//   } catch (output) {
+//     t.equal(output.exitCode, 1, 'CLI Exit Code should be 1');
+//     t.ok(
+//       output.stdout.includes(`${chalk.red('fail')}: http.response_time.p95 < 1`)
+//     );
+//     t.ok(output.stdout.includes(`${chalk.green('ok')}: p99 < 10000`));
 
-    const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
-    t.equal(
-      report.aggregate.counters['vusers.completed'],
-      10,
-      'Should have 10 total VUs'
-    );
-    t.equal(
-      report.aggregate.counters['http.codes.200'],
-      10,
-      'Should have 10 "200 OK" responses'
-    );
-  }
-});
+//     const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
+//     t.equal(
+//       report.aggregate.counters['vusers.completed'],
+//       10,
+//       'Should have 10 total VUs'
+//     );
+//     t.equal(
+//       report.aggregate.counters['http.codes.200'],
+//       10,
+//       'Should have 10 "200 OK" responses'
+//     );
+//   }
+// });
 
-test('CLI should exit with non-zero exit code when there are failed expectations in workers', async (t) => {
-  try {
-    await $`${A9} run-fargate ${__dirname}/fixtures/cli-exit-conditions/with-expect.yml --record --tags ${baseTags} --output ${reportFilePath} --count 2`;
-  } catch (output) {
-    t.equal(output.exitCode, 6, 'CLI Exit Code should be 6');
+// test('CLI should exit with non-zero exit code when there are failed expectations in workers', async (t) => {
+//   try {
+//     await $`${A9} run-fargate ${__dirname}/fixtures/cli-exit-conditions/with-expect.yml --record --tags ${baseTags} --output ${reportFilePath} --count 2`;
+//   } catch (output) {
+//     t.equal(output.exitCode, 6, 'CLI Exit Code should be 6');
 
-    const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
-    t.equal(
-      report.aggregate.counters['vusers.completed'],
-      10,
-      'Should have 10 total VUs'
-    );
-    t.equal(
-      report.aggregate.counters['http.codes.200'],
-      10,
-      'Should have 10 "200 OK" responses'
-    );
-  }
-});
+//     const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
+//     t.equal(
+//       report.aggregate.counters['vusers.completed'],
+//       10,
+//       'Should have 10 total VUs'
+//     );
+//     t.equal(
+//       report.aggregate.counters['http.codes.200'],
+//       10,
+//       'Should have 10 "200 OK" responses'
+//     );
+//   }
+// });
 
-test('Kitchen Sink Test - multiple features together', async (t) => {
-  const scenarioPath = `${__dirname}/fixtures/cli-kitchen-sink/kitchen-sink.yml`;
-  const dotEnvPath = `${__dirname}/fixtures/cli-kitchen-sink/kitchen-sink-env`;
-  const launchConfig = {
-    environment: [
-      { name: 'SECRET1', value: '/armadillo' },
-      { name: 'SECRET2', value: '/pony' }
-    ]
-  };
+// test('Kitchen Sink Test - multiple features together', async (t) => {
+//   const scenarioPath = `${__dirname}/fixtures/cli-kitchen-sink/kitchen-sink.yml`;
+//   const dotEnvPath = `${__dirname}/fixtures/cli-kitchen-sink/kitchen-sink-env`;
+//   const launchConfig = {
+//     environment: [
+//       { name: 'SECRET1', value: '/armadillo' },
+//       { name: 'SECRET2', value: '/pony' }
+//     ]
+//   };
 
-  const output =
-    await $`${A9} run-fargate ${scenarioPath} --output ${reportFilePath} --dotenv ${dotEnvPath} --record --tags ${baseTags} --count 2 --launch-config ${JSON.stringify(
-      launchConfig
-    )}`;
+//   const output =
+//     await $`${A9} run-fargate ${scenarioPath} --output ${reportFilePath} --dotenv ${dotEnvPath} --record --tags ${baseTags} --count 2 --launch-config ${JSON.stringify(
+//       launchConfig
+//     )}`;
 
-  t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
-  t.ok(
-    output.stdout.includes(
-      `${chalk.green('ok')}: http.response_time.p99 < 10000`
-    )
-  );
-  t.ok(output.stdout.includes(`${chalk.green('ok')}: p99 < 10000`));
+//   t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
+//   t.ok(
+//     output.stdout.includes(
+//       `${chalk.green('ok')}: http.response_time.p99 < 10000`
+//     )
+//   );
+//   t.ok(output.stdout.includes(`${chalk.green('ok')}: p99 < 10000`));
 
-  const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
-  t.equal(
-    report.aggregate.counters['vusers.completed'],
-    40,
-    'Should have 40 total VUs'
-  );
-  t.equal(
-    report.aggregate.counters['http.codes.200'],
-    160,
-    'Should have 160 "200 OK" responses'
-  );
+//   const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
+//   t.equal(
+//     report.aggregate.counters['vusers.completed'],
+//     40,
+//     'Should have 40 total VUs'
+//   );
+//   t.equal(
+//     report.aggregate.counters['http.codes.200'],
+//     160,
+//     'Should have 160 "200 OK" responses'
+//   );
 
-  // Check that each endpoint was hit correctly
-  t.equal(
-    report.aggregate.counters['plugins.metrics-by-endpoint./.codes.200'],
-    40,
-    'Should have 40 / "200 OK" responses'
-  );
-  t.equal(
-    report.aggregate.counters['plugins.metrics-by-endpoint./dino.codes.200'],
-    40,
-    'Should have 40 /dino "200 OK" responses'
-  );
-  t.equal(
-    report.aggregate.counters[
-      'plugins.metrics-by-endpoint./armadillo.codes.200'
-    ],
-    40,
-    'Should have 40 /armadillo "200 OK" responses'
-  );
-  t.equal(
-    report.aggregate.counters['plugins.metrics-by-endpoint./pony.codes.200'],
-    40,
-    'Should have 40 /pony "200 OK" responses'
-  );
-});
+//   // Check that each endpoint was hit correctly
+//   t.equal(
+//     report.aggregate.counters['plugins.metrics-by-endpoint./.codes.200'],
+//     40,
+//     'Should have 40 / "200 OK" responses'
+//   );
+//   t.equal(
+//     report.aggregate.counters['plugins.metrics-by-endpoint./dino.codes.200'],
+//     40,
+//     'Should have 40 /dino "200 OK" responses'
+//   );
+//   t.equal(
+//     report.aggregate.counters[
+//       'plugins.metrics-by-endpoint./armadillo.codes.200'
+//     ],
+//     40,
+//     'Should have 40 /armadillo "200 OK" responses'
+//   );
+//   t.equal(
+//     report.aggregate.counters['plugins.metrics-by-endpoint./pony.codes.200'],
+//     40,
+//     'Should have 40 /pony "200 OK" responses'
+//   );
+// });
 
-test('Run lots-of-output', async (t) => {
-  $.verbose = false; // we don't want megabytes of output on the console
+// test('Run lots-of-output', async (t) => {
+//   $.verbose = false; // we don't want megabytes of output on the console
 
-  const output =
-    await $`${A9} run:fargate ${__dirname}/fixtures/large-output/lots-of-output.yml --record --tags ${baseTags}`;
+//   const output =
+//     await $`${A9} run:fargate ${__dirname}/fixtures/large-output/lots-of-output.yml --record --tags ${baseTags}`;
 
-  t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
+//   t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
 
-  t.match(output.stdout, /summary report/i, 'print summary report');
-  t.match(
-    output.stdout,
-    /very.very.long.name.for.a.histogram.metric.so.that.we.generate.a.lot.of.console.output/i,
-    'includes custom metric output'
-  );
-  t.match(output.stdout, /p99/i, 'a p99 value is reported');
-});
+//   t.match(output.stdout, /summary report/i, 'print summary report');
+//   t.match(
+//     output.stdout,
+//     /very.very.long.name.for.a.histogram.metric.so.that.we.generate.a.lot.of.console.output/i,
+//     'includes custom metric output'
+//   );
+//   t.match(output.stdout, /p99/i, 'a p99 value is reported');
+// });
 
-test('Run memory hog', async (t) => {
-  try {
-    await $`${A9} run-fargate ${__dirname}/fixtures/memory-hog/memory-hog.yml --record --tags ${baseTags} --region us-east-1 --launch-config '{"cpu":"4096", "memory":"12288"}'`;
-  } catch (output) {
-    t.equal(output.exitCode, 6, 'CLI Exit Code should be 6');
+// test('Run memory hog', async (t) => {
+//   try {
+//     await $`${A9} run-fargate ${__dirname}/fixtures/memory-hog/memory-hog.yml --record --tags ${baseTags} --region us-east-1 --launch-config '{"cpu":"4096", "memory":"12288"}'`;
+//   } catch (output) {
+//     t.equal(output.exitCode, 6, 'CLI Exit Code should be 6');
 
-    t.match(output, /summary report/i, 'print summary report');
-    t.match(output, /p99/i, 'a p99 value is reported');
-  }
-});
+//     t.match(output, /summary report/i, 'print summary report');
+//     t.match(output, /p99/i, 'a p99 value is reported');
+//   }
+// });
