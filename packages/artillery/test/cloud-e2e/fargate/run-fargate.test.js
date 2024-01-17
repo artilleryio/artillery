d59c15c9b29b2b44986a5cfc0e8ef43021250be4
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-01-17 11:54:43 +0000

    refactor: try with execute instead

--- packages/artillery/test/cloud-e2e/fargate/run-fargate.test.js
+++ packages/artillery/test/cloud-e2e/fargate/run-fargate.test.js
@@ -2,7 +2,11 @@ const { test, before, beforeEach } = require('tap');
 const { $ } = require('zx');
 const chalk = require('chalk');
 const fs = require('fs');
-const { generateTmpReportPath, getTestTags } = require('../../cli/_helpers.js');
+const {
+  generateTmpReportPath,
+  getTestTags,
+  execute
+} = require('../../cli/_helpers.js');
 
 const A9 = process.env.A9 || 'artillery';
 
@@ -22,40 +26,58 @@ beforeEach(async (t) => {
 test('Run simple-bom', async (t) => {
   const scenarioPath = `${__dirname}/fixtures/simple-bom/simple-bom.yml`;
   console.log('scenarioPath', scenarioPath);
-  const output =
-    await $`${A9} run-fargate ${__dirname}/fixtures/simple-bom/simple-bom.yml --environment test --region eu-west-1 --count 9 --record --tags ${baseTags}`;
+  // const output =
+  //   await $`${A9} run-fargate ${__dirname}/fixtures/simple-bom/simple-bom.yml --environment test --region eu-west-1 --count 9 --record --tags ${baseTags}`;
 
-  t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
+  const [exitCode, output] = await execute([
+    'run-fargate',
+    '--environment',
+    'test',
+    '--region',
+    'eu-west-1',
+    '--count',
+    '9',
+    '--tags',
+    baseTags,
+    '--record',
+    scenarioPath
+  ]);
 
-  t.match(output, /summary report/i, 'print summary report');
-  t.match(output, /p99/i, 'a p99 value is reported');
-  t.match(output, /created:.+510/i, 'expected number of vusers is reported');
-});
+  t.equal(exitCode, 0, 'CLI Exit Code should be 0');
 
-test('Run mixed-hierarchy', async (t) => {
-  const scenarioPath = `${__dirname}/fixtures/mixed-hierarchy/scenarios/mixed-hierarchy-dino.yml`;
-  const configPath = `${__dirname}/fixtures/mixed-hierarchy/config/config.yml`;
-  console.log('scenarioPath', scenarioPath);
-  console.log('configPath', configPath);
-
-  const output =
-    await $`${A9} run-fargate ${scenarioPath} --config ${configPath} -e main --record --tags ${baseTags} --output ${reportFilePath}`;
-
-  const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
-
-  t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
-
-  t.equal(
-    report.aggregate.counters['vusers.completed'],
-    20,
-    'Should have 20 total VUs'
-  );
-  t.equal(
-    report.aggregate.counters['http.codes.200'],
-    20,
-    'Should have 20 "200 OK" responses'
+  t.match(output.stdout, /summary report/i, 'print summary report');
+  t.match(output.stdout, /p99/i, 'a p99 value is reported');
+  t.match(
+    output.stdout,
+    /created:.+510/i,
+    'expected number of vusers is reported'
   );
 });
+
+// test('Run mixed-hierarchy', async (t) => {
+//   const scenarioPath = `${__dirname}/fixtures/mixed-hierarchy/scenarios/mixed-hierarchy-dino.yml`;
+//   const configPath = `${__dirname}/fixtures/mixed-hierarchy/config/config.yml`;
+//   console.log('scenarioPath', scenarioPath);
+//   console.log('configPath', configPath);
+
+//   const output =
+//     await $`${A9} run-fargate ${scenarioPath} --config ${configPath} -e main --record --tags ${baseTags} --output ${reportFilePath}`;
+
+//   const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
+
+//   t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
+
+//   t.equal(
+//     report.aggregate.counters['vusers.completed'],
+//     20,
+//     'Should have 20 total VUs'
+//   );
+//   t.equal(
+//     report.aggregate.counters['http.codes.200'],
+//     20,
+//     'Should have 20 "200 OK" responses'
+//   );
+// });
 
 // test('Run uses ensure', async (t) => {
 //   try {
