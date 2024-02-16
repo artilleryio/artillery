edc94a96a0180a255ed8a98eac91b2395db46338
Bernardo Guerreiro <39738771+bernardobridge@users.noreply.github.com>
2024-02-16 13:54:38 +0000

    feat(cli): allow setting ts packages as external to improve compatibility (#2488)
    * feat(cli): set @playwright/test package as external by default on ts support
    
    * dep(playwright): add @playwright/test as dependency by default
    
    * feat(cli): expose bundling option to set external packages for ts
    
    * test(cli): add ts externals local test
    
    * test(fargate): add fargate test with ts and external pkg

--- packages/artillery/test/cli/run-typescript.test.js
+++ packages/artillery/test/cli/run-typescript.test.js
@@ -1,6 +1,11 @@
 const tap = require('tap');
-const { execute, generateTmpReportPath } = require('../cli/_helpers.js');
+const {
+  execute,
+  generateTmpReportPath,
+  deleteFile
+} = require('../cli/_helpers.js');
 const fs = require('fs');
+const path = require('path');
 
 let reportFilePath;
 tap.beforeEach(async (t) => {
@@ -32,6 +37,63 @@ tap.test('Can run a Typescript processor', async (t) => {
     2,
     'Should have emitted 2 custom metrics from ts processor'
   );
+});
+
+tap.test('Runs correctly when package is marked as external', async (t) => {
+  const override = JSON.stringify({
+    config: {
+      bundling: {
+        external: ['lodash']
+      }
+    }
+  });
+
+  const [exitCode, output] = await execute(
+    [
+      'run',
+      '-o',
+      `${reportFilePath}`,
+      'test/scripts/scenarios-typescript/lodash.yml',
+      '--overrides',
+      override
+    ],
+    {
+      env: { ARTILLERY_TS_KEEP_BUNDLE: true, extendEnv: true }
+    }
+  );
+
+  //assert that test ran successfully
+  t.equal(exitCode, 0, 'CLI should exit with code 0');
+  t.ok(
+    output.stdout.includes('Got context using lodash: true'),
+    'Should be able to use lodash in a scenario to get context'
+  );
+  const json = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
+
+  t.equal(
+    json.aggregate.counters['http.codes.200'],
+    2,
+    'Should have made 2 requests'
+  );
+  t.equal(
+    json.aggregate.counters['hey_from_ts'],
+    2,
+    'Should have emitted 2 custom metrics from ts processor'
+  );
+
+  //assert that the bundle was created and marked as external
+  const bundleLocation = path.join(
+    path.dirname('test/scripts/scenarios-typescript/lodash.yml'),
+    'dist/processor.js'
+  );
+  t.ok(fs.existsSync(bundleLocation), 'Bundle should exist');
+  const bundle = fs.readFileSync(bundleLocation, 'utf8');
+  t.ok(
+    bundle.includes('require("lodash")'),
+    'Bundle should require lodash instead of bundling it'
+  );
+
+  await deleteFile(bundleLocation);
 });
 
 tap.test(
