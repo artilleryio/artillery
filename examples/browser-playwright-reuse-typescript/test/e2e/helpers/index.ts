4ebf54563094c1ef942ac4fd4405bfb97a6161dc
Bernardo Guerreiro <39738771+bernardobridge@users.noreply.github.com>
2024-02-27 14:12:13 +0000

    feat(examples): add example for reusing pw typescript tests (#2513)
    * feat(examples): add example for reusing pw typescript tests
    
    * chore: readme change

---
+++ examples/browser-playwright-reuse-typescript/test/e2e/helpers/index.ts
@@ -0,0 +1,29 @@
+import { Page, expect } from '@playwright/test';
+
+export const goToDocsAndSearch = async (page: Page, step) => {
+  await step('go_to_artillery_io', async () => {
+    await page.goto('/');
+  });
+
+  await step('go_to_docs', async () => {
+    await page
+      .getByLabel('Main navigation')
+      .getByRole('link', { name: 'Documentation' })
+      .click();
+    await expect(page).toHaveURL('/docs');
+    await expect(
+      page.getByText("What's different about Artillery?")
+    ).toBeVisible();
+  });
+
+  await step('search_for_ts_doc_and_goto', async () => {
+    await page
+      .getByRole('searchbox', { name: 'Search documentation…' })
+      .click();
+    await page.keyboard.type('typescript', { delay: 100 });
+    await page
+      .getByRole('link', { name: 'processor - custom JS code' })
+      .click();
+    await expect(page.getByText('processor - custom JS code')).toBeVisible();
+  });
+};
