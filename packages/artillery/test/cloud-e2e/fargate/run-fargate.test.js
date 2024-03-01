f9d7fcb5a92f1ce6dc2411d7090c2b3466752db1
Bernardo Guerreiro <39738771+bernardobridge@users.noreply.github.com>
2024-03-01 09:39:42 +0000

    fix(cli): remove default values for cpu/memory flags (#2526)

--- packages/artillery/test/cloud-e2e/fargate/run-fargate.test.js
+++ packages/artillery/test/cloud-e2e/fargate/run-fargate.test.js
@@ -267,13 +267,31 @@ test('Run lots-of-output', async (t) => {
   t.match(output.stdout, /p99/i, 'a p99 value is reported');
 });
 
-test('Run memory hog', async (t) => {
+test('Fargate should exit with error code when workers run out of memory', async (t) => {
   try {
-    await $`${A9} run-fargate ${__dirname}/fixtures/memory-hog/memory-hog.yml --record --tags ${baseTags} --region us-east-1 --launch-config '{"cpu":"4096", "memory":"12288"}'`;
+    await $`${A9} run-fargate ${__dirname}/fixtures/memory-hog/memory-hog.yml --record --tags ${baseTags},should_fail:true --region us-east-1`;
   } catch (output) {
     t.equal(output.exitCode, 6, 'CLI Exit Code should be 6');
 
     t.match(output, /summary report/i, 'print summary report');
     t.match(output, /p99/i, 'a p99 value is reported');
   }
+});
+
+test('Fargate should not run out of memory when cpu and memory is increased via launch config', async (t) => {
+  const output =
+    await $`${A9} run-fargate ${__dirname}/fixtures/memory-hog/memory-hog.yml --record --tags ${baseTags},should_fail:false --region us-east-1 --launch-config '{"cpu":"4096", "memory":"12288"}'`;
+
+  t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
+  t.match(output, /summary report/i, 'print summary report');
+  t.match(output, /p99/i, 'a p99 value is reported');
+});
+
+test('Fargate should not run out of memory when cpu and memory is increased via flags', async (t) => {
+  const output =
+    await $`${A9} run-fargate ${__dirname}/fixtures/memory-hog/memory-hog.yml --record --tags ${baseTags},should_fail:false --region us-east-1 --cpu 4 --memory 12`;
+
+  t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
+  t.match(output, /summary report/i, 'print summary report');
+  t.match(output, /p99/i, 'a p99 value is reported');
 });
