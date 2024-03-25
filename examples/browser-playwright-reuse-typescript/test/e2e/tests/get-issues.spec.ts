5086662ac75f8517fb6a1775b64ef7d5b924fc48
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-03-25 09:03:00 +0000

    fix(examples): pass test.step correctly to playwright test helper

--- examples/browser-playwright-reuse-typescript/test/e2e/tests/get-issues.spec.ts
+++ examples/browser-playwright-reuse-typescript/test/e2e/tests/get-issues.spec.ts
@@ -2,5 +2,5 @@ import { goToDocsAndSearch } from '../helpers';
 import { test } from '@playwright/test';
 
 test('search and go to doc page', async ({ page }) => {
-  await goToDocsAndSearch(page, test);
+  await goToDocsAndSearch(page, test.step);
 });
