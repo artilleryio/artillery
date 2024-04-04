601f787b9e44ee094c79fc69d1258802d0e157cb
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-04-04 20:22:43 +0100

    test(core): fix config variables quarantined test

--- packages/core/test/acceptance/config-variables.test.js
+++ packages/core/test/acceptance/config-variables.test.js
@@ -1,18 +1,35 @@
 'use strict';
 
-const { test } = require('tap');
+const { test, beforeEach, afterEach } = require('tap');
 const runner = require('../..').runner.runner;
 const { SSMS } = require('../../lib/ssms');
+const createTestServer = require('../targets/simple');
 
-test('config variables', function (t) {
+let server;
+let port;
+beforeEach(async () => {
+  server = await createTestServer(0);
+  port = server.info.port;
+});
+
+afterEach(() => {
+  server.stop();
+});
+
+test('config variables', (t) => {
   const script = require('../scripts/config_variables.json');
+  script.config.target = `http://127.0.0.1:${port}`;
+
   runner(script).then(function (ee) {
     ee.on('done', function (nr) {
       const report = SSMS.legacyReport(nr).report();
       t.ok(report.codes[200] > 0, 'there are 200s for /');
       t.ok(report.codes[404] > 0, 'there are 404s for /will/404');
-      t.end();
+      ee.stop().then(() => {
+        t.end();
+      });
     });
+
     ee.run();
   });
 });
