0e5483309104f6eae62614fa3fd67cacae3dc180
Nidhi Work <fazlic.ines@gmail.com>
2024-04-15 10:05:30 +0100

    test(metrics-by-endpoint): add test for parallel

--- packages/artillery-plugin-metrics-by-endpoint/test/index.spec.js
+++ packages/artillery-plugin-metrics-by-endpoint/test/index.spec.js
@@ -44,3 +44,37 @@ test('reports in console and json report correctly', async (t) => {
     )
   );
 });
+
+test("Reports correctly when 'parallel' is used", async (t) => {
+  const expectedVus = 4;
+  const expectedVusFailed = 0;
+  const requestPaths = ['/dino', '/pony', '/armadillo'];
+
+  const reportPath =
+    os.tmpdir() + '/artillery-plugin-metrics-by-endpoint-parallel-test.json';
+  const output =
+    await $`../artillery/bin/run run ./test/fixtures/scenario-parallel.yml -o ${reportPath}`;
+
+  const report = require(reportPath);
+
+  t.equal(output.exitCode, 0, 'CLI Exit Code should be 0');
+  t.equal(
+    report.aggregate.counters['vusers.created'],
+    expectedVus,
+    `${expectedVus} VUs should have been created`
+  );
+  t.equal(
+    report.aggregate.counters['vusers.failed'],
+    expectedVusFailed,
+    `${expectedVusFailed} VUs should have failed`
+  );
+  for (const path of requestPaths) {
+    t.equal(
+      report.aggregate.counters[
+        `plugins.metrics-by-endpoint.${path}.codes.200`
+      ],
+      expectedVus,
+      `${expectedVus} requests to ${path} should have returned 200`
+    );
+  }
+});
