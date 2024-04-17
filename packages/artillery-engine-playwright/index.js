3e3e2d624fed14f00ef3739f66a4485e442ae45d
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-04-17 19:34:20 +0100

    feat: add playwright support

--- packages/artillery-engine-playwright/index.js
+++ packages/artillery-engine-playwright/index.js
@@ -1,6 +1,53 @@
 const debug = require('debug')('engine:playwright');
 const { chromium, selectors } = require('playwright');
 
+const additionalChromeFlagsForLambda = [
+  '--enable-precise-memory-info',
+  '--disable-dev-shm-usage',
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-gpu',
+  '--single-process'
+  // '--in-process-gpu'
+
+  //"--autoplay-policy=user-gesture-required",
+  //"--disable-background-networking",
+  //"--disable-background-timer-throttling",
+  //"--disable-backgrounding-occluded-windows",
+  //"--disable-breakpad",
+  //"--disable-client-side-phishing-detection",
+  //"--disable-component-update",
+  //"--disable-default-apps",
+  //"--disable-dev-shm-usage",
+  //"--disable-domain-reliability",
+  //"--disable-extensions",
+  //"--disable-features=AudioServiceOutOfProcess",
+  //"--disable-hang-monitor",
+  //"--disable-ipc-flooding-protection",
+  //"--disable-notifications",
+  //"--disable-offer-store-unmasked-wallet-cards",
+  //"--disable-popup-blocking",
+  //"--disable-print-preview",
+  //"--disable-prompt-on-repost",
+  //"--disable-renderer-backgrounding",
+  //"--disable-setuid-sandbox",
+  //"--disable-speech-api",
+  //"--disable-sync",
+  //"--disk-cache-size=33554432",
+  //"--hide-scrollbars",
+  //"--ignore-gpu-blacklist",
+  //"--metrics-recording-only",
+  //"--mute-audio",
+  //"--no-default-browser-check",
+  //"--no-first-run",
+  //"--no-pings",
+  //"--no-sandbox",
+  //"--no-zygote",
+  //"--password-store=basic",
+  //"--use-gl=swiftshader",
+  //"--use-mock-keychain",
+];
+
 class PlaywrightEngine {
   constructor(script) {
     debug('constructor');
@@ -118,6 +165,10 @@ class PlaywrightEngine {
           );
           launchOptions.headless = true;
         }
+
+        //TODO: review this and make it for Lambda only
+        // https://blog.carlosnunez.me/post/scraping-chromium-lambda-nodeless-zerostress/
+        launchOptions.args = { ...additionalChromeFlagsForLambda };
       }
 
       const contextOptions = {
