9616b97c80228b00e0cd3921d76c8c5eb19814fa
Nidhi Work <fazlic.ines@gmail.com>
2024-04-23 16:51:55 +0100

    feat(cli): add deprecation notice to report cmd

--- packages/artillery/lib/cmds/report.js
+++ packages/artillery/lib/cmds/report.js
@@ -7,6 +7,7 @@ const telemetry = require('../telemetry').init();
 
 class ReportCommand extends Command {
   async run() {
+    console.log(deprecationNotice);
     telemetry.capture('report generate');
     const { flags, args } = await this.parse(ReportCommand);
     const output = flags.output || args.file + '.html'; // TODO: path.resolve
@@ -34,6 +35,21 @@ ReportCommand.flags = {
     description: 'Write HTML report to specified location'
   })
 };
+
+const deprecationNotice = `
+\x1b[34m┌───────────────────────────────────────────────────────────────────────┐
+│                          \x1b[33mDEPRECATION NOTICE                           \x1b[34m│
+├───────────────────────────────────────────────────────────────────────┤
+│ \x1b[0m\x1b[33mThe "report" command is deprecated and will be removed in a future\x1b[0m    \x1b[34m│
+│ \x1b[0m\x1b[33mrelease of Artillery.\x1b[0m                                                 \x1b[34m│
+│                                                                       │
+│ \x1b[0m\x1b[37m\x1b[94mArtillery Cloud\x1b[0m is now the recommended way to visualize test results. \x1b[0m\x1b[34m│
+│ \x1b[0m\x1b[37mIt provides more comprehensive reporting, advanced visualizations,\x1b[0m    \x1b[34m│
+│ \x1b[0m\x1b[37mand includes a free tier.                         \x1b[0m                    \x1b[34m│
+│                                                                       │
+│ \x1b[0m\x1b[37mSign up on \x1b[0m \x1b[4m\x1b[36mhttps://artillery.io/cloud\x1b[24m                                \x1b[34m│
+└───────────────────────────────────────────────────────────────────────┘
+\x1b[0m`;
 
 ReportCommand.args = { file: Args.string() };
 
