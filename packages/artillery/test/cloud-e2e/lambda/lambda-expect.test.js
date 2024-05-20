c1fd1feab2f3162ecbc26232d6b44548a87af847
Bernardo Guerreiro <39738771+bernardobridge@users.noreply.github.com>
2024-05-20 21:44:35 +0100

    fix(lambda): handle cli nonzero codes from workers (#2766)

---
+++ packages/artillery/test/cloud-e2e/lambda/lambda-expect.test.js
@@ -0,0 +1,42 @@
+const tap = require('tap');
+const { $ } = require('zx');
+const chalk = require('chalk');
+const fs = require('fs');
+const { generateTmpReportPath, getTestTags } = require('../../cli/_helpers.js');
+
+const tags = getTestTags(['type:acceptance']);
+let reportFilePath;
+tap.beforeEach(async (t) => {
+  reportFilePath = generateTmpReportPath(t.name, 'json');
+});
+
+tap.test(
+  'CLI should exit with non-zero exit code when there are failed expectations in container workers',
+  async (t) => {
+    try {
+      await $`artillery run-lambda ${__dirname}/../fargate/fixtures/cli-exit-conditions/with-expect.yml --record --tags ${tags} --output ${reportFilePath} --container --count 2`;
+      t.fail(`Test "${t.name}" - Should have had non-zero exit code.`);
+    } catch (output) {
+      t.equal(output.exitCode, 1, 'CLI Exit Code should be 1');
+
+      const report = JSON.parse(fs.readFileSync(reportFilePath, 'utf8'));
+      t.equal(
+        report.aggregate.counters['vusers.completed'],
+        10,
+        'Should have 10 total VUs'
+      );
+
+      t.equal(
+        report.aggregate.counters['plugins.expect.failed'],
+        10,
+        'Should have 20 failed expectations'
+      );
+
+      t.equal(
+        report.aggregate.counters['http.codes.200'],
+        10,
+        'Should have 10 "200 OK" responses'
+      );
+    }
+  }
+);
