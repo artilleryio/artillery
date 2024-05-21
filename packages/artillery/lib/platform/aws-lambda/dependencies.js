aeba7c9396579ec8c732f3d2ce1a353fd20dab47
Bernardo Guerreiro <39738771+bernardobridge@users.noreply.github.com>
2024-05-21 10:51:36 +0100

    fix(lambda): convert to posix paths before sending files to s3 (#2767)
    * fix(lambda): convert to posix paths before sending files to s3
    
    * fix(lambda): use posix on lambda args too

--- packages/artillery/lib/platform/aws-lambda/dependencies.js
+++ packages/artillery/lib/platform/aws-lambda/dependencies.js
@@ -240,7 +240,7 @@ async function _uploadFileToS3(item, testRunId, bucketName) {
     return;
   }
 
-  const key = prefix + '/' + item.noPrefix;
+  const key = prefix + '/' + item.noPrefixPosix;
 
   try {
     await s3
