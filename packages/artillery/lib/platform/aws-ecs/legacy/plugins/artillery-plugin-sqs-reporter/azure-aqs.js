b8d61b0d858cfc3e9651a5d976cf4699cdf803e6
hveldstra <h@veldstra.org>
2024-07-04 16:40:23 +0100

    feat: extend loadgen-worker with Azure support

---
+++ packages/artillery/lib/platform/aws-ecs/legacy/plugins/artillery-plugin-sqs-reporter/azure-aqs.js
@@ -0,0 +1,27 @@
+// Copyright (c) Artillery Software Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+const { QueueClient } = require('@azure/storage-queue');
+const { DefaultAzureCredential } = require('@azure/identity');
+
+function getAQS() {
+  return new QueueClient(
+    process.env.AZURE_STORAGE_QUEUE_URL,
+    new DefaultAzureCredential()
+  );
+}
+
+function sendMessage(queue, body, tags) {
+  const payload = JSON.stringify({
+    payload: body,
+    // attributes: this.tags
+    attributes: tags.reduce((acc, tag) => {
+      acc[tag.key] = tag.value;
+      return acc;
+    }, {})
+  });
+
+  return queue.sendMessage(payload);
+}
+
+module.exports = { getAQS, sendMessage };
